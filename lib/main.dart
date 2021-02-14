601e64fc50d7b1d7063d4fcd8a6cf4e021c68a78
Nikunj Das Kasat <nikunjdaskasat@gmail.com>
2021-02-14 14:42:54 +0530

    Added internet permission in the main AndroidManifest.xml

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       navigatorKey: locator<NavigationService>().navigatorKey,
       onGenerateRoute: router.Router.generateRoute,
       initialRoute: splashViewRoute,
