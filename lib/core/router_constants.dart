2c562bcee1231ad22232098fd531fc68961c52a0
Nikunj Das Kasat <nikunjdaskasat@gmail.com>
2021-03-13 23:34:17 +0530

    Removed Stacked

--- lib/core/router_constants.dart
+++
@@ -1,9 +0,0 @@
-// [ This is an auto generated file ]
-
-const String splashViewRoute = 'splashViewRoute';
-const String trainingViewRoute = 'trainingViewRoute';
-const String forumViewRoute = 'forumViewRoute';
-const String newsViewRoute = 'newsViewRoute';
-const String radioViewRoute = 'radioViewRoute';
-const String donationViewRoute = 'donationViewRoute';
-const String faqViewRoute = 'faqViewRoute';
--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,160 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:stacked_services/stacked_services.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-import 'core/locator.dart';
-import 'core/router_constants.dart';
-import 'core/router.dart' as router;
-
-Future main() async {
-  await LocatorInjector.setUpLocator();
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      navigatorKey: locator<NavigationService>().navigatorKey,
-      onGenerateRoute: router.Router.generateRoute,
-      initialRoute: splashViewRoute,
       title: 'freeCodeCamp',
-      supportedLocales: const <Locale>[
-        Locale('en', 'US'),
-        Locale('es', 'ES'),
-        Locale('zh', 'CN')
-      ],
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  WebViewController? controller;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF0a0a23),
+        actions: [
+          Row(
+            children: <Widget>[
+              IconButton(
+                icon: const Icon(Icons.arrow_back_ios),
+                onPressed: () async {
+                  if (await controller!.canGoBack()) {
+                    await controller!.goBack();
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text("No back history item")));
+                    return;
+                  }
+                },
+              ),
+              IconButton(
+                icon: const Icon(Icons.arrow_forward_ios),
+                onPressed: () async {
+                  if (await controller!.canGoForward()) {
+                    await controller!.goForward();
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text("No forward history item")));
+                    return;
+                  }
+                },
+              ),
+              IconButton(
+                icon: const Icon(Icons.replay),
+                onPressed: () {
+                  controller!.reload();
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            DrawerHeader(
+              child: Text(''),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/images/splash_screen.png')),
+              ),
+            ),
+            ListTile(
+                title: Text(
+                  'Training',
+                  style: TextStyle(color: Color(0xFF0a0a23)),
+                ),
+                onTap: () {
+                  controller!.loadUrl('https://www.freecodecamp.org/learn/');
+                  Navigator.pop(context);
+                }),
+            ListTile(
+                title: Text(
+                  'Forum',
+                  style: TextStyle(color: Color(0xFF0a0a23)),
+                ),
+                onTap: () {
+                  controller!.loadUrl('https://forum.freecodecamp.org/');
+                  Navigator.pop(context);
+                }),
+            ListTile(
+                title: Text(
+                  'News',
+                  style: TextStyle(color: Color(0xFF0a0a23)),
+                ),
+                onTap: () {
+                  controller!.loadUrl('https://www.freecodecamp.org/news/');
+                  Navigator.pop(context);
+                }),
+            ListTile(
+                title: Text(
+                  'Radio',
+                  style: TextStyle(color: Color(0xFF0a0a23)),
+                ),
+                onTap: () {
+                  controller!.loadUrl('https://coderadio.freecodecamp.org/');
+                  Navigator.pop(context);
+                }),
+            ListTile(
+              title: Text(
+                'FAQ',
+                style: TextStyle(color: Color(0xFF0a0a23)),
+              ),
+              onTap: () {
+                controller!.loadUrl('https://www.freecodecamp.org/news/about/');
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text(
+                'Donate',
+                style: TextStyle(color: Color(0xFF0a0a23)),
+              ),
+              onTap: () {
+                controller!.loadUrl('https://www.freecodecamp.org/donate');
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+      body: WillPopScope(
+        onWillPop: () async {
+          if (await controller!.canGoBack()) {
+            await controller!.goBack();
+          } else {
+            ScaffoldMessenger.of(context)
+                .showSnackBar(SnackBar(content: Text("No back history item")));
+          }
+          return false;
+        },
+        child: WebView(
+          initialUrl: 'https://www.freecodecamp.org/learn/',
+          userAgent: "random",
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController webViewController) {
+            setState(() {
+              controller = webViewController;
+            });
+          },
+        ),
+      ),
     );
   }
 }
