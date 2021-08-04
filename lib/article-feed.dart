493688eccbdb10ea26afe4c8bf1cb6a9a2acae41
Sem Bauke <semboot699@gmail.com>
2021-08-04 14:03:30 +0200

    feat: make articles displayable in app

--- lib/article-feed.dart
+++ lib/article-feed.dart
@@ -4,10 +4,10 @@ import 'dart:developer' as dev;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'article-view.dart';
 
-int page = 1;
+int page = 0;
 
 class ArticleApp extends StatefulWidget {
   const ArticleApp({Key? key}) : super(key: key);
@@ -22,6 +22,21 @@ truncateStr(str) {
   } else {
     return str.toString().substring(0, 55) + '...';
   }
+}
+
+randomBorderColor() {
+  final random = new Random();
+
+  List borderColor = [
+    Color.fromRGBO(0x99, 0xC9, 0xFF, 1),
+    Color.fromRGBO(0xAC, 0xD1, 0x57, 1),
+    Color.fromRGBO(0xFF, 0xFF, 0x00, 1),
+    Color.fromRGBO(0x80, 0x00, 0x80, 1),
+  ];
+
+  int index = random.nextInt(borderColor.length);
+
+  return borderColor[index];
 }
 
 class Article {
@@ -56,6 +71,7 @@ class _ArticleAppState extends State<ArticleApp> {
   void dispose() {
     _scrollController.dispose();
     super.dispose();
+    page = 0;
   }
 
   @override
@@ -96,7 +112,7 @@ class _ArticleAppState extends State<ArticleApp> {
     String feedUrl =
         "https://www.freecodecamp.org/news/ghost/api/v3/content/posts/?key=${dotenv.env['NEWSKEY']}&include=tags,authors&page=" +
             page.toString() +
-            "&fields=title,url,feature_image";
+            "&fields=title,url,feature_image,id";
     dev.log(feedUrl);
     final response = await http.get(Uri.parse(feedUrl));
     if (response.statusCode == 200) {
@@ -133,12 +149,18 @@ class ArticleTemplate extends StatelessWidget {
                 color: Color(0xFF0a0a23),
                 height: 100,
                 child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: new InkWell(
-                      child: new Text(truncateStr(articels[i]["title"]),
-                          style: TextStyle(fontSize: 24, color: Colors.white)),
-                      onTap: () => launch(articels[i]["url"])),
-                ),
+                    padding: const EdgeInsets.all(16.0),
+                    child: new InkWell(
+                        child: new Text(truncateStr(articels[i]["title"]),
+                            style:
+                                TextStyle(fontSize: 24, color: Colors.white)),
+                        onTap: () => {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => ArticleViewTemplate(
+                                          articleId: articels[i]["id"])))
+                            })),
               ),
             ),
           ],
@@ -184,21 +206,6 @@ class ArticleBanner extends StatelessWidget {
   final List articels;
   final int i;
 
-  randomBorderColor() {
-    final random = new Random();
-
-    List borderColor = [
-      Color.fromRGBO(0x99, 0xC9, 0xFF, 1),
-      Color.fromRGBO(0xAC, 0xD1, 0x57, 1),
-      Color.fromRGBO(0xFF, 0xFF, 0x00, 1),
-      Color.fromRGBO(0x80, 0x00, 0x80, 1),
-    ];
-
-    int index = random.nextInt(borderColor.length);
-
-    return borderColor[index];
-  }
-
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -208,7 +215,13 @@ class ArticleBanner extends StatelessWidget {
             decoration: BoxDecoration(
                 border: Border.all(width: 2, color: Colors.white)),
             child: GestureDetector(
-                onTap: () => launch(articels[i]["url"]),
+                onTap: () => {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ArticleViewTemplate(
+                                  articleId: articels[i]["id"])))
+                    },
                 child: Image.network(
                   articels[i]["feature_image"],
                   height: 210,
---
+++ lib/article-view.dart
@@ -0,0 +1,161 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+import 'article-feed.dart' as border;
+
+Future<Article> fetchArticle(articleId) async {
+  await dotenv.load(fileName: ".env");
+
+  final response = await http.get(Uri.parse(
+      'https://www.freecodecamp.org/news/ghost/api/v3/content/posts/$articleId/?key=${dotenv.env['NEWSKEY']}&include=authors&formats=plaintext '));
+
+  if (response.statusCode == 200) {
+    return Article.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to load article');
+  }
+}
+
+class Article {
+  final String author;
+  final String authorImage;
+  final String aritcleTitle;
+  final String articleText;
+  final String articleImage;
+
+  Article(
+      {required this.author,
+      required this.authorImage,
+      required this.aritcleTitle,
+      required this.articleText,
+      required this.articleImage});
+
+  factory Article.fromJson(Map<String, dynamic> json) {
+    return Article(
+        author: json['posts'][0]['primary_author']['name'],
+        authorImage: json['posts'][0]['primary_author']['profile_image'],
+        aritcleTitle: json['posts'][0]['title'],
+        articleText: json['posts'][0]['plaintext'],
+        articleImage: json['posts'][0]['feature_image']);
+  }
+}
+
+class ArticleViewTemplate extends StatefulWidget {
+  ArticleViewTemplate({Key? key, required this.articleId}) : super(key: key);
+
+  final String articleId;
+
+  _ArticleAppState createState() => _ArticleAppState();
+}
+
+class _ArticleAppState extends State<ArticleViewTemplate> {
+  late Future<Article> futureArticle;
+  @override
+  void initState() {
+    super.initState();
+    futureArticle = fetchArticle(widget.articleId);
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar:
+            AppBar(title: Text('NEWSFEED'), backgroundColor: Color(0xFF0a0a23)),
+        backgroundColor: Color(0xFF0a0a23),
+        body: SingleChildScrollView(
+            child: Column(children: [
+          FutureBuilder<Article>(
+              future: futureArticle,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Column(
+                    children: [
+                      Row(
+                        children: [
+                          Expanded(
+                              child: Image.network(
+                            snapshot.data!.articleImage,
+                            height: 250,
+                            fit: BoxFit.fitHeight,
+                          )),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Container(
+                            color: Color.fromRGBO(0x2A, 0x2A, 0x40, 1),
+                            width: MediaQuery.of(context).size.width,
+                            child: Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Text(snapshot.data!.aritcleTitle,
+                                  style: TextStyle(
+                                      fontSize: 24, color: Colors.white)),
+                            ),
+                          )
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Container(
+                              color: Color.fromRGBO(0x2A, 0x2A, 0x40, 1),
+                              child: Row(
+                                children: [
+                                  Padding(
+                                    padding: EdgeInsets.all(16),
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                          border: Border.all(
+                                              width: 4,
+                                              color:
+                                                  border.randomBorderColor())),
+                                      child: Image.network(
+                                        snapshot.data!.authorImage,
+                                        width: 50,
+                                        height: 50,
+                                      ),
+                                    ),
+                                  ),
+                                  Expanded(
+                                      child: Container(
+                                    color: Color.fromRGBO(0x2A, 0x2A, 0x40, 1),
+                                    child: Padding(
+                                      padding: EdgeInsets.only(top: 8),
+                                      child: Text(
+                                        'Written by ' + snapshot.data!.author,
+                                        style: TextStyle(
+                                            fontSize: 16, color: Colors.white),
+                                      ),
+                                    ),
+                                  ))
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Text(
+                                snapshot.data!.articleText,
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 16),
+                              ),
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  );
+                } else if (snapshot.hasError) {
+                  return Text('${snapshot.error}');
+                }
+                return const CircularProgressIndicator();
+              })
+        ])));
+  }
+}
