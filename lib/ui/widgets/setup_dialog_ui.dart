b20a40f86ef53666c2829465141a2b4c01999315
Niraj Nandish <nirajnandish@icloud.com>
2021-11-11 09:12:48 +0400

    fix: missing types and improve ui

--- lib/ui/widgets/setup_dialog_ui.dart
+++ lib/ui/widgets/setup_dialog_ui.dart
@@ -8,9 +8,21 @@ void setupDialogUi() {
   final dialogService = locator<DialogService>();
 
   final builders = {
-    DialogType.basic: (contex, sheetRequest, completer) =>
-        _BasicDialog(request: sheetRequest, completer: completer),
-    DialogType.authform: (contex, sheetRequest, completer) => _AuthFormDialog(
+    DialogType.basic: (
+      BuildContext context,
+      DialogRequest sheetRequest,
+      Function(DialogResponse) completer,
+    ) =>
+        _BasicDialog(
+          request: sheetRequest,
+          completer: completer,
+        ),
+    DialogType.authform: (
+      BuildContext context,
+      DialogRequest sheetRequest,
+      Function(DialogResponse) completer,
+    ) =>
+        _AuthFormDialog(
           request: sheetRequest,
           onDialogTap: completer,
         )
@@ -42,97 +54,104 @@ class _AuthFormDialog extends HookWidget {
   Widget build(BuildContext context) {
     final authCodeController = useTextEditingController();
     return Dialog(
-        backgroundColor: const Color(0xFF0a0a23),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(32.0),
-              child: Row(
-                children: [
-                  Text(
-                    request.title as String,
-                    style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold),
+      backgroundColor: const Color(0xFF0a0a23),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: Row(
+              children: [
+                Text(
+                  request.title as String,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.only(left: 32, top: 16, right: 32.0),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Text(
-                      request.description as String,
-                      style: const TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 32, right: 32.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    request.description as String,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.only(left: 32, top: 16, right: 32.0),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: SizedBox(
-                      height: 100,
-                      child: TextField(
-                        controller: authCodeController,
-                        maxLength: 6,
-                        autofocus: true,
-                        keyboardType: TextInputType.number,
-                        style: const TextStyle(color: Colors.white),
-                        decoration: InputDecoration(
-                            helperText: 'Please enter a six digit code',
-                            helperStyle: const TextStyle(color: Colors.white),
-                            enabledBorder: const OutlineInputBorder(
-                                borderSide:
-                                    BorderSide(color: Colors.white, width: 2)),
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(0)),
-                            label: const Text(
-                              'Code',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold),
-                            )),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 32, top: 16, right: 32.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: SizedBox(
+                    height: 100,
+                    child: TextField(
+                      controller: authCodeController,
+                      maxLength: 6,
+                      autofocus: true,
+                      keyboardType: TextInputType.number,
+                      style: const TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        helperText: 'Please enter the six digit code',
+                        helperStyle: const TextStyle(color: Colors.white),
+                        enabledBorder: const OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.white, width: 2)),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(0)),
+                        label: const Text(
+                          'Code',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
                       ),
                     ),
-                  )
-                ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.only(left: 32, top: 8, right: 32, bottom: 32),
+            child: SizedBox(
+              height: 50,
+              width: MediaQuery.of(context).size.width,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    primary: const Color.fromRGBO(0x3b, 0x3b, 0x4f, 1),
+                    side: const BorderSide(width: 2, color: Colors.white),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(0))),
+                onPressed: () => {
+                  onDialogTap(DialogResponse(
+                      data: authCodeController.text, confirmed: true)),
+                  onDialogTap(DialogResponse())
+                },
+                child: const Text(
+                  'LOGIN',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white),
+                ),
               ),
             ),
-            Padding(
-              padding:
-                  const EdgeInsets.only(left: 32, top: 8, right: 32, bottom: 8),
-              child: SizedBox(
-                height: 50,
-                width: MediaQuery.of(context).size.width,
-                child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        primary: const Color.fromRGBO(0x3b, 0x3b, 0x4f, 1),
-                        side: const BorderSide(width: 2, color: Colors.white),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(0))),
-                    onPressed: () => {
-                          onDialogTap(DialogResponse(
-                              data: authCodeController.text, confirmed: true)),
-                          onDialogTap(DialogResponse())
-                        },
-                    child: const Text(
-                      'LOGIN',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(color: Colors.white),
-                    )),
-              ),
-            ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 }
