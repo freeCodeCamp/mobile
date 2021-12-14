15a86a3148ce85af5fef414d642f73e8eb3a1e6f
Mehade <mehade369@gmail.com>
2021-12-15 00:33:27 +0600

    moved variables outside build method

--- mobile-app/lib/ui/views/home/home_view.dart
+++ mobile-app/lib/ui/views/home/home_view.dart
@@ -10,19 +10,20 @@ import 'home_viemmodel.dart';
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
 
+  static const views = <Widget>[
+    NewsBookmarkFeedView(),
+    NewsFeedView(),
+    NewsSearchView()
+    //const ArticleSearch()
+  ];
+  static const titles = <Widget>[
+    Text('BOOKMARKED ARTICLES'),
+    Text('NEWSFEED'),
+    Text('SEARCH ARTICLES')
+  ];
+
   @override
   Widget build(BuildContext context) {
-    List<dynamic> views = <dynamic>[
-      const NewsBookmarkFeedView(),
-      const NewsFeedView(),
-      const NewsSearchView()
-      //const ArticleSearch()
-    ];
-    List<Widget> titles = <Widget>[
-      const Text('BOOKMARKED ARTICLES'),
-      const Text('NEWSFEED'),
-      const Text('SEARCH ARTICLES')
-    ];
     return ViewModelBuilder<HomeViewModel>.reactive(
       builder: (context, viewModel, child) => Scaffold(
         appBar: AppBar(
@@ -34,30 +35,36 @@ class HomeView extends StatelessWidget {
           width: MediaQuery.of(context).size.width,
           child: const DrawerWidgetView(),
         ),
-        body: views.elementAt(viewModel.index),
+        body: IndexedStack(
+          index: viewModel.index,
+          children: views,
+        ),
         bottomNavigationBar: BottomNavigationBar(
           backgroundColor: const Color(0xFF0a0a23),
           unselectedItemColor: Colors.white,
           selectedItemColor: const Color.fromRGBO(0x99, 0xc9, 0xff, 1),
           items: const <BottomNavigationBarItem>[
             BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.bookmark_outline_sharp,
-                  color: Colors.white,
-                ),
-                label: 'Bookmarks'),
+              icon: Icon(
+                Icons.bookmark_outline_sharp,
+                color: Colors.white,
+              ),
+              label: 'Bookmarks',
+            ),
             BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.article_sharp,
-                  color: Colors.white,
-                ),
-                label: 'Articles'),
+              icon: Icon(
+                Icons.article_sharp,
+                color: Colors.white,
+              ),
+              label: 'Articles',
+            ),
             BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.search_sharp,
-                  color: Colors.white,
-                ),
-                label: 'Search')
+              icon: Icon(
+                Icons.search_sharp,
+                color: Colors.white,
+              ),
+              label: 'Search',
+            )
           ],
           currentIndex: viewModel.index,
           onTap: viewModel.onTapped,
