ed0c4076746b60fd0d52fa8fabf45cc19f4692c0
Niraj Nandish <nirajnandish@icloud.com>
2021-12-27 23:42:53 +0400

    fix: add comment textController

--- mobile-app/lib/ui/views/forum/forum-post/forum_post_view.dart
+++ mobile-app/lib/ui/views/forum/forum-post/forum_post_view.dart
@@ -236,7 +236,7 @@ class ForumPostView extends StatelessWidget {
           ],
         ),
         ForumTextFunctionBar(
-          textController: model.commentText,
+          textController: model.createPostText,
           post: post,
         ),
         model.commentHasError
