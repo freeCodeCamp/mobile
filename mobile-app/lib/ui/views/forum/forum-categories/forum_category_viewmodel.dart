98e452468017f01f067a3f25750a538a04532b39
SemBauke <semboot699@gmail.com>
2022-02-22 13:26:00 +0100

    feat: only fetch profile when logged in

--- mobile-app/lib/ui/views/forum/forum-categories/forum_category_viewmodel.dart
+++ mobile-app/lib/ui/views/forum/forum-categories/forum_category_viewmodel.dart
@@ -66,7 +66,11 @@ class ForumCategoryViewModel extends BaseViewModel {
     }
 
     _isLoggedIn = await checkLoggedIn();
-    _user = await fetchUser();
+
+    if (_isLoggedIn) {
+      _user = await fetchUser();
+    }
+
     notifyListeners();
     setupDialogUi();
 
