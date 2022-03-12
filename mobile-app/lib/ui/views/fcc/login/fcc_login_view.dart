b65b503d68c89e33d3688db6106f552596b02055
SemBauke <semboot699@gmail.com>
2022-03-12 11:04:35 +0100

    feat: add login buttons

--- mobile-app/lib/ui/views/fcc/login/fcc_login_view.dart
+++ mobile-app/lib/ui/views/fcc/login/fcc_login_view.dart
@@ -12,7 +12,12 @@ class FccLoginView extends StatelessWidget {
         builder: (context, model, child) => Scaffold(
               body: SafeArea(
                 child: Column(
-                  children: [loginHeader()],
+                  children: [
+                    loginHeader(),
+                    loginButton('Google'),
+                    loginButton('GitHub'),
+                    orWidget()
+                  ],
                 ),
               ),
             ));
@@ -39,8 +44,56 @@ class FccLoginView extends StatelessWidget {
                 child: Text(
               'Login to your freeCodeCamp account',
               textAlign: TextAlign.center,
-              style: TextStyle(height: 2.5),
+              style: TextStyle(height: 2.5, fontSize: 20),
             ))
+          ],
+        )
+      ],
+    );
+  }
+
+  Widget loginButton(String type) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                border: Border.all(width: 2, color: Colors.white)),
+            width: 300,
+            height: 50,
+            child: ElevatedButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: const Color.fromRGBO(0x3b, 0x3b, 0x4f, 1),
+                ),
+                onPressed: () {},
+                child: Text('Continue with $type')),
+          )
+        ],
+      ),
+    );
+  }
+
+  Column orWidget() {
+    return Column(
+      children: [
+        Stack(
+          alignment: Alignment.center,
+          children: const [
+            SizedBox(
+                width: 300,
+                child: Divider(
+                  color: Colors.white,
+                  thickness: 1,
+                )),
+            Text(
+              'or',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                backgroundColor: Color.fromRGBO(0x2A, 0x2A, 0x40, 1),
+              ),
+            )
           ],
         )
       ],
