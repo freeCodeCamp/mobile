699f9046618aef4c69d4fb80844c01eb6f834221
SemBauke <semboot699@gmail.com>
2022-05-11 11:02:35 +0200

    feat: add more parameters

--- mobile-app/lib/models/main/user_model.dart
+++ mobile-app/lib/models/main/user_model.dart
@@ -19,6 +19,10 @@ class FccUserModel {
   final bool isCheater;
   final bool acceptedPrivacyTerms;
   final bool sendQuincyEmail;
+  final bool isDonating;
+
+  final String? emailAuthLinkTTL;
+  final String? emailVerifyTTL;
 
   final String username;
   final String about;
@@ -44,4 +48,70 @@ class FccUserModel {
   final bool isDataAnalysisPyCertV7;
   final bool isMachineLearningPyCertV7;
   final bool isRelationalDatabaseCertV8;
+
+  final List badges;
+
+  final ProfileUI profileUI;
+
+  FccUserModel(
+      this._id,
+      this.email,
+      this.emailVerified,
+      this.isBanned,
+      this.isCheater,
+      this.acceptedPrivacyTerms,
+      this.sendQuincyEmail,
+      this.username,
+      this.about,
+      this.name,
+      this.picture,
+      this.currentChallengeId,
+      this.isHonest,
+      this.isFrontEndCert,
+      this.isDataVisCert,
+      this.isBackEndCert,
+      this.isFullStackCert,
+      this.isRespWebDesignCert,
+      this.is2018DataVisCert,
+      this.isFrontEndLibsCert,
+      this.isJsAlgoDataStructCert,
+      this.isApisMicroservicesCert,
+      this.isInfosecQaCert,
+      this.isQaCertV7,
+      this.isInfosecCertV7,
+      this.is2018FullStackCert,
+      this.isSciCompPyCertV7,
+      this.isDataAnalysisPyCertV7,
+      this.isMachineLearningPyCertV7,
+      this.isRelationalDatabaseCertV8,
+      this.profileUI,
+      this.isDonating,
+      this.badges,
+      this.emailAuthLinkTTL,
+      this.emailVerifyTTL);
+}
+
+class ProfileUI {
+  final bool isLocked;
+  final bool showAbout;
+  final bool showCerts;
+  final bool showDonation;
+  final bool showHeatMap;
+  final bool showLocation;
+  final bool showName;
+  final bool showPoints;
+  final bool showPortfolio;
+  final bool showTimeLine;
+
+  ProfileUI(
+      this.isLocked,
+      this.showAbout,
+      this.showCerts,
+      this.showDonation,
+      this.showHeatMap,
+      this.showLocation,
+      this.showName,
+      this.showPoints,
+      this.showPortfolio,
+      this.showTimeLine);
 }
