4383b9e598a903209547c7dc2a1aa84588b5306c
SemBauke <semboot699@gmail.com>
2022-05-18 08:57:55 +0200

    feat: display quotes

--- mobile-app/lib/ui/views/learn/learn_view.dart
+++ mobile-app/lib/ui/views/learn/learn_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:freecodecamp/models/learn/motivational_quote_model.dart';
 import 'package:freecodecamp/ui/views/learn/learn_viewmodel.dart';
 import 'package:freecodecamp/ui/widgets/drawer_widget/drawer_widget_view.dart';
 import 'package:stacked/stacked.dart';
@@ -26,6 +27,7 @@ class LearnView extends StatelessWidget {
                   if (snapshot.hasData) {
                     var superBlocks = snapshot.data as List;
                     return ListView(shrinkWrap: true, children: [
+                      quouteWidget(),
                       !model.auth.isLoggedIn ? loginButton(model) : Container(),
                       ListView.builder(
                           physics: const ClampingScrollPhysics(),
@@ -99,18 +101,49 @@ class LearnView extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.only(top: 16, left: 8, right: 8),
       height: 75,
-      child: Expanded(
-        child: ElevatedButton(
-            style: ElevatedButton.styleFrom(
-                primary: const Color.fromRGBO(0xf1, 0xbe, 0x32, 1)),
-            onPressed: () {
-              model.auth.login();
-            },
-            child: const Text(
-              'Sign in to save your progress ',
-              style: TextStyle(color: Colors.black, fontSize: 20),
-            )),
-      ),
+      child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              primary: const Color.fromRGBO(0xf1, 0xbe, 0x32, 1)),
+          onPressed: () {
+            model.auth.login();
+          },
+          child: const Text(
+            'Sign in to save your progress ',
+            style: TextStyle(color: Colors.black, fontSize: 20),
+          )),
     );
+  }
+
+  Widget quouteWidget() {
+    LearnViewModel model = LearnViewModel();
+
+    return FutureBuilder(
+        future: model.retrieveNewQuote(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            MotivationalQuote quote = snapshot.data as MotivationalQuote;
+
+            return Container(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '"${quote.quote}"',
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(fontSize: 20, height: 1.5),
+                  ),
+                  Text(
+                    '- ${quote.author}',
+                    style: const TextStyle(
+                        fontStyle: FontStyle.italic, fontSize: 16, height: 1.5),
+                  )
+                ],
+              ),
+            );
+          }
+
+          return Container();
+        });
   }
 }
