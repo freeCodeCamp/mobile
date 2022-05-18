1d500489eed1748d43c5ef7691fc6a8d72d74fd2
Niraj Nandish <nirajnandish@icloud.com>
2022-05-18 15:00:12 +0400

    show available user details

--- mobile-app/lib/ui/views/profile/profile_viemodel.dart
+++ mobile-app/lib/ui/views/profile/profile_viemodel.dart
@@ -1,10 +1,20 @@
 import 'dart:async';
+import 'dart:developer';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:freecodecamp/models/main/user_model.dart';
 import 'package:freecodecamp/service/authentication_service.dart';
 import 'package:stacked/stacked.dart';
 import 'package:freecodecamp/app/app.locator.dart';
 
 // import 'dart:developer';
 class ProfileViewModel extends BaseViewModel {
-  void initState() async {}
+  final AuthenticationService _authenticationService =
+      locator<AuthenticationService>();
+  late FccUserModel user;
+
+  void initState() async {
+    user = _authenticationService.userModel!;
+    log(user.toString());
+    notifyListeners();
+  }
 }
--- mobile-app/lib/ui/views/profile/profile_view.dart
+++ mobile-app/lib/ui/views/profile/profile_view.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:freecodecamp/ui/views/profile/profile_viemodel.dart';
 import 'package:freecodecamp/ui/widgets/drawer_widget/drawer_widget_view.dart';
@@ -17,8 +18,36 @@ class ProfileView extends StatelessWidget {
           title: const Text('PROFILE'),
         ),
         drawer: const DrawerWidgetView(),
-        body: const Center(
-          child: Text('Profile view coming here'),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CachedNetworkImage(
+                imageUrl: model.user.picture,
+                height: MediaQuery.of(context).size.width * 0.5,
+                width: MediaQuery.of(context).size.width * 0.5,
+                errorWidget: (context, url, error) => const Icon(Icons.error),
+                imageBuilder: (context, imageProvider) => Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: imageProvider,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+              Text('@${model.user.username}'),
+              Text(model.user.name),
+              // Text(model.user.location),
+              model.user.about != null ? Text(model.user.about!) : Container(),
+              // Row(children: [Icon(Icons.calendar_month),Text(model.user.joinDate)]),
+              // TODO: Top Contributor comes here
+              // Text('${model.user.points} total points'),
+              // TODO: Add heatmap here
+              // TODO: Certifications here
+              // TODO: Timeline here
+            ],
+          ),
         ),
       ),
     );
