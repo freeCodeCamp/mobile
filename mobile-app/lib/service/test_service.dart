d2f2c949615db5acc9aad13baea710dae3a0188c
Niraj Nandish <nirajnandish@icloud.com>
2022-05-30 16:28:19 +0400

    chore: remove log statement
    Co-authored-by: Sem Bauke <46919888+Sembauke@users.noreply.github.com>

--- mobile-app/lib/service/test_service.dart
+++ mobile-app/lib/service/test_service.dart
@@ -4,7 +4,6 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class TestService {
   Future<bool> developmentMode() async {
-    log('Checking if in development mode');
     await dotenv.load();
     return dotenv.get('DEVELOPMENTMODE', fallback: '').toLowerCase() == 'true';
   }
