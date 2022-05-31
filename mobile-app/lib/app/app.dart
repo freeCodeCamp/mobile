488ce29a5a3b5c7419e86ad669ad49082c1d3d1b
Sem Bauke <46919888+Sembauke@users.noreply.github.com>
2022-05-31 08:28:05 +0200

    chore: remove old auth view (#375)
    * chore: remove old auth dummy
    
    * fix: routing

--- mobile-app/lib/app/app.dart
+++ mobile-app/lib/app/app.dart
@@ -21,7 +21,6 @@ import 'package:freecodecamp/ui/views/news/news-bookmark/news_bookmark_view.dart
 import 'package:freecodecamp/ui/views/news/news-feed/news_feed_view.dart';
 import 'package:freecodecamp/ui/views/settings/forumSettings/forum_settings_view.dart';
 import 'package:freecodecamp/ui/views/settings/podcastSettings/podcast_settings_view.dart';
-import 'package:freecodecamp/ui/views/auth/auth_view.dart';
 import 'package:freecodecamp/ui/views/profile/profile_view.dart';
 
 import 'package:freecodecamp/ui/views/news/news-image-viewer/news_image_viewer.dart';
@@ -48,7 +47,6 @@ import 'package:sqflite_migration_service/sqflite_migration_service.dart';
     MaterialRoute(page: ForumSettingsView),
     MaterialRoute(page: ForumUserProfileView),
     MaterialRoute(page: CodeRadioView),
-    MaterialRoute(page: AuthView),
     MaterialRoute(page: ProfileView)
   ],
   dependencies: [
