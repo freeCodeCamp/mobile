dd1f2da445e7c4e307418688eec33dba83bafa6d
Niraj Nandish <nirajnandish@icloud.com>
2022-06-18 22:39:25 +0400

    setup audio handler

--- mobile-app/lib/main.dart
+++ mobile-app/lib/main.dart
@@ -1,10 +1,10 @@
+import 'package:audio_service/audio_service.dart';
 import 'package:fk_user_agent/fk_user_agent.dart';
 import 'package:flutter/material.dart';
+import 'package:freecodecamp/service/audio_service.dart';
 import 'package:freecodecamp/service/authentication_service.dart';
-import 'package:freecodecamp/service/code_radio_service.dart';
 import 'package:freecodecamp/service/notification_service.dart';
 import 'package:freecodecamp/ui/theme/fcc_theme.dart';
-import 'package:just_audio_background/just_audio_background.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 import 'app/app.locator.dart';
@@ -12,18 +12,20 @@ import 'app/app.router.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await JustAudioBackground.init(
-    androidNotificationChannelId: 'com.ryanheise.bg_demo.channel.audio',
-    androidNotificationChannelName: 'Audio playback',
-    androidNotificationOngoing: true,
-  );
   await AuthenticationService().init();
   await NotificationService().init();
   await FkUserAgent.init();
+  await AudioService.init(
+    builder: () => AudioPlayerHandler(),
+    config: const AudioServiceConfig(
+      androidNotificationChannelId: 'org.freecodecamp.channel.audio',
+      androidNotificationChannelName: 'Audio playback',
+      androidNotificationOngoing: true,
+      androidStopForegroundOnPause: true,
+    ),
+  );
 
   setupLocator();
-  final audioService = locator<CodeRadioService>();
-  audioService.initAppStateObserver();
   runApp(const MyApp());
 }
 
---
+++ mobile-app/lib/service/audio_service.dart
@@ -0,0 +1,105 @@
+import 'dart:developer';
+
+import 'package:freecodecamp/models/podcasts/episodes_model.dart';
+import 'package:freecodecamp/models/podcasts/podcasts_model.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:audio_service/audio_service.dart';
+
+// May not be required as service is instantiated in main.dart and attached
+// to below Audio Player Handler.
+// class AppAudioService {
+//   static final AppAudioService _audioService = AppAudioService._internal();
+
+//   late AudioHandler _audioHandler;
+
+//   factory AppAudioService() {
+//     return _audioService;
+//   }
+
+//   Future<void> init() async {
+//     _audioHandler = await AudioService.init(
+//       builder: () => AudioPlayerHandler(),
+//       config: const AudioServiceConfig(
+//         androidNotificationChannelId: 'org.freecodecamp.channel.audio',
+//         androidNotificationChannelName: 'Audio playback',
+//         androidNotificationOngoing: true,
+//         androidStopForegroundOnPause: true,
+//       ),
+//     );
+//   }
+
+//   AppAudioService._internal();
+// }
+
+class AudioPlayerHandler extends BaseAudioHandler {
+  final AudioPlayer _audioPlayer = AudioPlayer();
+
+  // Future<void> play(Episodes episode, bool isDownloaded) async {
+  @override
+  Future<void> play() async {
+    // if (_audioPlayer.playerState.processingState == ProcessingState.idle) {
+    //   await loadEpisode(episode, isDownloaded);
+    // }
+    // if (episode.id != _audioPlayer.audioSource?.sequence[0].tag.id) {
+    //   log('DIFFERENT EPISODE');
+    //   await _audioPlayer.stop();
+    //   await loadEpisode(episode, isDownloaded);
+    // }
+    _audioPlayer.play();
+  }
+
+  @override
+  Future<void> pause() async {
+    await _audioPlayer.pause();
+  }
+
+  @override
+  Future<void> stop() async {
+    await _audioPlayer.stop();
+    playbackState.add(playbackState.value.copyWith(
+      processingState: AudioProcessingState.idle,
+    ));
+  }
+
+  Future<void> loadEpisode(
+    Episodes episode,
+    bool isDownloaded,
+    Podcasts podcast,
+  ) async {
+    try {
+      if (isDownloaded) {
+        await _audioPlayer.setAudioSource(
+          AudioSource.uri(
+            Uri.parse(
+                'file:///data/user/0/org.freecodecamp/app_flutter/episodes/${episode.podcastId}/${episode.id}.mp3'),
+            tag: MediaItem(
+              id: episode.id,
+              title: episode.title,
+              album: podcast.title,
+              artUri: Uri.parse(
+                  'file:///data/user/0/org.freecodecamp/app_flutter/images/podcast/${episode.podcastId}.jpg'),
+            ),
+          ),
+          preload: false,
+        );
+      } else {
+        await _audioPlayer.setAudioSource(
+          AudioSource.uri(
+            Uri.parse(episode.contentUrl!),
+            tag: MediaItem(
+              id: episode.id,
+              title: episode.title,
+              album: podcast.title,
+              artUri: Uri.parse(
+                  'file:///data/user/0/org.freecodecamp/app_flutter/images/podcast/${episode.podcastId}.jpg'),
+            ),
+          ),
+          preload: false,
+        );
+      }
+      await _audioPlayer.load();
+    } catch (e) {
+      log('Cannot play audio: $e');
+    }
+  }
+}
