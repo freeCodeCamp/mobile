f71575aefef0658ab29c79014f990a1a982247f4
Niraj Nandish <nirajnandish@icloud.com>
2022-06-29 08:51:34 +0400

    fix: remove episode description view more

--- mobile-app/lib/ui/views/podcast/episode-view/episode_viewmodel.dart
+++ mobile-app/lib/ui/views/podcast/episode-view/episode_viewmodel.dart
@@ -1,11 +1,3 @@
 import 'package:stacked/stacked.dart';
 
-class EpisodeViewModel extends BaseViewModel {
-  bool _viewMoreDescription = false;
-  bool get viewMoreDescription => _viewMoreDescription;
-
-  set setViewMoreDescription(bool state) {
-    _viewMoreDescription = state;
-    notifyListeners();
-  }
-}
+class EpisodeViewModel extends BaseViewModel {}
