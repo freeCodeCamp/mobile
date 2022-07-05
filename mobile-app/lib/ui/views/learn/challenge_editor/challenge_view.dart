a1d6edc85a4cf5fb16429bac84e4554476f64ffb
SemBauke <semboot699@gmail.com>
2022-07-05 14:41:13 +0200

    feat: new learn ui

--- mobile-app/lib/ui/views/learn/challenge_editor/challenge_view.dart
+++ mobile-app/lib/ui/views/learn/challenge_editor/challenge_view.dart
@@ -2,10 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_code_editor/controller/editor_view_controller.dart';
 import 'package:flutter_code_editor/controller/language_controller/syntax/index.dart';
 import 'package:flutter_code_editor/models/editor_options.dart';
-import 'package:flutter_code_editor/models/file_model.dart';
-import 'package:freecodecamp/models/learn/challenge_model.dart';
 import 'package:freecodecamp/ui/views/learn/challenge_editor/challenge_model.dart';
-import 'package:freecodecamp/ui/views/learn/challenge_editor/description/description_view.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:stacked/stacked.dart';
 
 class ChallengeView extends StatelessWidget {
@@ -19,35 +17,47 @@ class ChallengeView extends StatelessWidget {
         viewModelBuilder: () => ChallengeModel(),
         onDispose: (model) => model.disposeCahce(url),
         builder: (context, model, child) => Scaffold(
-                body: FutureBuilder(
+            bottomNavigationBar: BottomAppBar(
+              color: const Color(0xFF0a0a23),
+              child: Row(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.all(8),
+                    color: const Color.fromRGBO(0x2A, 0x2A, 0x40, 1),
+                    child: IconButton(
+                      icon: const FaIcon(FontAwesomeIcons.info),
+                      onPressed: () => {},
+                    ),
+                  ),
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.all(8),
+                          color: const Color.fromRGBO(0x1D, 0x9B, 0xF0, 1),
+                          child: IconButton(
+                            icon: const FaIcon(FontAwesomeIcons.check),
+                            onPressed: () => {},
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            body: FutureBuilder(
               future: model.initChallenge(url),
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
-                  // TODO: Check if challenge is null
-                  Challenge? challenge = snapshot.data as Challenge?;
+                  // Challenge? challenge = snapshot.data as Challenge?;
                   EditorViewController controller = EditorViewController(
                     language: Syntax.HTML,
-                    options: EditorOptions(
-                        useFileExplorer: false,
-                        canCloseFiles: false,
-                        customViewNames: [
-                          const Text('Description'),
-                        ],
-                        customViews: [
-                          DescriptionView(
-                            description: challenge!.description,
-                            instructions: challenge.instructions,
-                            tests: challenge.tests,
-                            editorText: model.editorText,
-                          )
-                        ]),
-                    recentlyOpenedFiles: model.returnFiles(challenge),
-                    file: FileIDE(
-                        fileExplorer: null,
-                        fileName: challenge.files[0].name,
-                        filePath: '',
-                        fileContent: challenge.files[0].contents,
-                        parentDirectory: ''),
+                    options: const EditorOptions(
+                      useFileExplorer: false,
+                      canCloseFiles: false,
+                    ),
                   );
 
                   controller.editorTextStream.stream.listen((event) {
