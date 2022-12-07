c43f915ee338a6318d50f49a82e61f7207ae2a96
sembauke <semboot699@gmail.com>
2022-12-07 14:33:00 +0100

    feat: better search news

--- mobile-app/lib/ui/views/news/news-search/news_search_view.dart
+++ mobile-app/lib/ui/views/news/news-search/news_search_view.dart
@@ -1,6 +1,7 @@
 // ignore: file_names
 import 'package:flutter/material.dart';
 import 'package:algolia/algolia.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:freecodecamp/ui/views/news/news-search/news_search_viewmodel.dart';
 
 import 'package:stacked/stacked.dart';
@@ -10,59 +11,83 @@ class NewsSearchView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<NewsSearchModel>.reactive(
-        viewModelBuilder: () => NewsSearchModel(),
-        builder: (context, model, child) => Scaffold(
-            appBar: AppBar(
-                title: TextField(
-              controller: model.searchbarController,
-              decoration: const InputDecoration(
-                  hintText: 'SEARCH TUTORIALS...',
-                  fillColor: Color.fromRGBO(0x2A, 0x2A, 0x40, 1),
-                  filled: true),
-              onChanged: (value) {
-                model.setSearchTerm(value);
+      viewModelBuilder: () => NewsSearchModel(),
+      builder: (context, model, child) => Scaffold(
+        appBar: AppBar(
+            title: TextField(
+          controller: model.searchbarController,
+          decoration: const InputDecoration(
+              hintText: 'SEARCH TUTORIALS...',
+              fillColor: Color.fromRGBO(0x2A, 0x2A, 0x40, 1),
+              filled: true),
+          onChanged: (value) {
+            model.setSearchTerm(value);
+          },
+        )),
+        body: StreamBuilder<List<AlgoliaObjectSnapshot>>(
+          stream: Stream.fromFuture(model.search(model.getSearchTerm)),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            if (snapshot.hasError) return Text('Error: ${snapshot.error}');
+
+            List<AlgoliaObjectSnapshot>? current = snapshot.data;
+
+            SchedulerBinding.instance.addPostFrameCallback(
+              (timeStamp) {
+                // model.handleArticleStepAmount(current!.length);
               },
-            )),
-            body: StreamBuilder<List<AlgoliaObjectSnapshot>>(
-              stream: Stream.fromFuture(model.search(model.getSearchTerm)),
-              builder: (context, snapshot) {
-                if (!snapshot.hasData) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
+            );
 
-                if (snapshot.hasError) return Text('Error: ${snapshot.error}');
-
-                List<AlgoliaObjectSnapshot>? current = snapshot.data;
-
-                return Column(
-                  children: [
-                    Expanded(
-                      child: ListView.separated(
-                          itemCount: current!.length,
-                          separatorBuilder: (context, int i) => const Divider(
-                                color: Color.fromRGBO(0x42, 0x42, 0x55, 1),
-                                thickness: 2,
-                                height: 5,
-                              ),
-                          itemBuilder: (context, index) {
-                            return ListTile(
-                              title: Text(
-                                current[index].data['title'],
-                                maxLines: 2,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                              onTap: () => {
-                                model.navigateToArticle(
-                                    current[index].data['objectID'])
-                              },
-                            );
-                          }),
-                    )
-                  ],
-                );
-              },
-            )));
+            return Column(
+              children: [
+                Expanded(
+                    child: current!.isNotEmpty
+                        ? ListView.separated(
+                            itemCount: model.viewedAmount,
+                            controller: model.controller,
+                            separatorBuilder: (context, int i) => const Divider(
+                              color: Color.fromRGBO(0x42, 0x42, 0x55, 1),
+                              thickness: 2,
+                              height: 5,
+                            ),
+                            itemBuilder: (context, index) {
+                              return ListTile(
+                                title: Text(
+                                  current[index].data['title'],
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextStyle(
+                                    color: Colors.white.withOpacity(0.87),
+                                  ),
+                                ),
+                                onTap: () => {
+                                  model.navigateToArticle(
+                                    current[index].data['objectID'],
+                                  ),
+                                },
+                              );
+                            },
+                          )
+                        : const Center(
+                            child: Text('No Tutorials Found'),
+                          )),
+                if (!model.hitMaxViewed)
+                  TextButton(
+                    onPressed: () {
+                      model.extendArticlesViewed();
+                    },
+                    child: const Text('press for more tutorials'),
+                  )
+              ],
+            );
+          },
+        ),
+      ),
+    );
   }
 }
--- mobile-app/lib/ui/views/news/news-search/news_search_viewmodel.dart
+++ mobile-app/lib/ui/views/news/news-search/news_search_viewmodel.dart
@@ -10,8 +10,42 @@ class NewsSearchModel extends BaseViewModel {
   String _searchTerm = '';
   String get getSearchTerm => _searchTerm;
 
+  int _maxArticleAmount = 21;
+  int get getMaxArticleAmount => _maxArticleAmount;
+
+  int _stepAmount = 7;
+
+  int _viewedAmount = 7;
+  int get viewedAmount => _viewedAmount;
+
+  bool _hitMaxViewed = false;
+  bool get hitMaxViewed => _hitMaxViewed;
+
+  set setViewedAmount(value) {
+    _viewedAmount = value;
+    notifyListeners();
+  }
+
+  set setHitMaxViewed(value) {
+    _hitMaxViewed = value;
+    notifyListeners();
+  }
+
+  set setMaxArticleAmount(value) {
+    _maxArticleAmount = value;
+    notifyListeners();
+  }
+
+  set setStepAmount(value) {
+    _stepAmount = value;
+    notifyListeners();
+  }
+
   final searchbarController = TextEditingController();
   final _navigationService = locator<NavigationService>();
+
+  ScrollController controller = ScrollController();
+
   Future<List<AlgoliaObjectSnapshot>> search(String inputQuery) async {
     await dotenv.load(fileName: '.env');
 
@@ -25,7 +59,7 @@ class NewsSearchModel extends BaseViewModel {
     AlgoliaQuery query = algolia.instance
         .index('news')
         .similarQuery(inputQuery.isEmpty ? 'JavaScript' : inputQuery)
-        .setHitsPerPage(7);
+        .setHitsPerPage(_maxArticleAmount);
 
     AlgoliaQuerySnapshot snap = await query.getObjects();
 
@@ -39,8 +73,35 @@ class NewsSearchModel extends BaseViewModel {
     notifyListeners();
   }
 
+  void handleArticleStepAmount(int articleAmount) {
+    _maxArticleAmount = articleAmount;
+
+    if (articleAmount <= 7) {
+      setViewedAmount = articleAmount;
+    } else {
+      setViewedAmount = articleAmount;
+    }
+  }
+
+  void extendArticlesViewed() {
+    // If the max article amount is already viewedAmount return and do nothing.
+    // else add 7 and check again.
+
+    if (_maxArticleAmount == viewedAmount) return;
+
+    setViewedAmount = _viewedAmount += _stepAmount;
+
+    if (_maxArticleAmount == viewedAmount) {
+      setHitMaxViewed = true;
+    }
+  }
+
   void navigateToArticle(id) {
-    _navigationService.navigateTo(Routes.newsArticleView,
-        arguments: NewsArticleViewArguments(refId: id));
+    _navigationService.navigateTo(
+      Routes.newsArticleView,
+      arguments: NewsArticleViewArguments(
+        refId: id,
+      ),
+    );
   }
 }
