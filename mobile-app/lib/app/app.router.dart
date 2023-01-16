8f108b2f18c3cd48c204bb93c60bb74475096e5a
sembauke <semboot699@gmail.com>
2023-01-16 16:17:42 +0100

    chore: update routing

--- mobile-app/lib/app/app.router.dart
+++ mobile-app/lib/app/app.router.dart
@@ -5,32 +5,20 @@
 // **************************************************************************
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:flutter/material.dart' as _i24;
+import 'package:flutter/material.dart' as _i18;
 import 'package:flutter/material.dart';
-import 'package:freecodecamp/models/learn/curriculum_model.dart' as _i28;
+import 'package:freecodecamp/models/learn/curriculum_model.dart' as _i22;
 import 'package:freecodecamp/models/news/bookmarked_tutorial_model.dart'
-    as _i27;
-import 'package:freecodecamp/models/podcasts/episodes_model.dart' as _i25;
-import 'package:freecodecamp/models/podcasts/podcasts_model.dart' as _i26;
-import 'package:freecodecamp/ui/views/code_radio/code_radio_view.dart' as _i18;
-import 'package:freecodecamp/ui/views/forum/forum-categories/forum_category_view.dart'
-    as _i11;
-import 'package:freecodecamp/ui/views/forum/forum-login/forum_login_view.dart'
-    as _i14;
-import 'package:freecodecamp/ui/views/forum/forum-post-feed/forum_post_feed_view.dart'
-    as _i12;
-import 'package:freecodecamp/ui/views/forum/forum-post/forum_post_view.dart'
-    as _i13;
-import 'package:freecodecamp/ui/views/forum/forum-user-profile/forum_user_profile_view.dart'
-    as _i17;
-import 'package:freecodecamp/ui/views/forum/forum-user/forum_user_view.dart'
-    as _i15;
+    as _i21;
+import 'package:freecodecamp/models/podcasts/episodes_model.dart' as _i19;
+import 'package:freecodecamp/models/podcasts/podcasts_model.dart' as _i20;
+import 'package:freecodecamp/ui/views/code_radio/code_radio_view.dart' as _i12;
 import 'package:freecodecamp/ui/views/home/home_view.dart' as _i2;
 import 'package:freecodecamp/ui/views/learn/challenge_editor/challenge_view.dart'
-    as _i20;
+    as _i14;
 import 'package:freecodecamp/ui/views/learn/learn-builders/superblock_builder.dart'
-    as _i19;
-import 'package:freecodecamp/ui/views/learn/learn/learn_view.dart' as _i23;
+    as _i13;
+import 'package:freecodecamp/ui/views/learn/learn/learn_view.dart' as _i17;
 import 'package:freecodecamp/ui/views/news/news-author/news_author_view.dart'
     as _i9;
 import 'package:freecodecamp/ui/views/news/news-bookmark/news_bookmark_view.dart'
@@ -44,14 +32,14 @@ import 'package:freecodecamp/ui/views/news/news-tutorial/news_tutorial_view.dart
 import 'package:freecodecamp/ui/views/podcast/episode/episode_view.dart' as _i5;
 import 'package:freecodecamp/ui/views/podcast/podcast-list/podcast_list_view.dart'
     as _i3;
-import 'package:freecodecamp/ui/views/profile/profile_view.dart' as _i21;
+import 'package:freecodecamp/ui/views/profile/profile_view.dart' as _i15;
 import 'package:freecodecamp/ui/views/settings/forumSettings/forum_settings_view.dart'
-    as _i16;
+    as _i11;
 import 'package:freecodecamp/ui/views/settings/podcastSettings/podcast_settings_view.dart'
     as _i4;
-import 'package:freecodecamp/ui/views/web_view/web_view_view.dart' as _i22;
+import 'package:freecodecamp/ui/views/web_view/web_view_view.dart' as _i16;
 import 'package:stacked/stacked.dart' as _i1;
-import 'package:stacked_services/stacked_services.dart' as _i29;
+import 'package:stacked_services/stacked_services.dart' as _i23;
 
 class Routes {
   static const homeView = '/';
@@ -72,19 +60,7 @@ class Routes {
 
   static const newsImageView = '/news-image-view';
 
-  static const forumCategoryView = '/forum-category-view';
-
-  static const forumPostFeedView = '/forum-post-feed-view';
-
-  static const forumPostView = '/forum-post-view';
-
-  static const forumLoginView = '/forum-login-view';
-
-  static const forumUserView = '/forum-user-view';
-
   static const forumSettingsView = '/forum-settings-view';
-
-  static const forumUserProfileView = '/forum-user-profile-view';
 
   static const codeRadioView = '/code-radio-view';
 
@@ -108,13 +84,7 @@ class Routes {
     newsFeedView,
     newsAuthorView,
     newsImageView,
-    forumCategoryView,
-    forumPostFeedView,
-    forumPostView,
-    forumLoginView,
-    forumUserView,
     forumSettingsView,
-    forumUserProfileView,
     codeRadioView,
     superBlockView,
     challengeView,
@@ -163,56 +133,32 @@ class StackedRouter extends _i1.RouterBase {
       page: _i10.NewsImageView,
     ),
     _i1.RouteDef(
-      Routes.forumCategoryView,
-      page: _i11.ForumCategoryView,
-    ),
-    _i1.RouteDef(
-      Routes.forumPostFeedView,
-      page: _i12.ForumPostFeedView,
-    ),
-    _i1.RouteDef(
-      Routes.forumPostView,
-      page: _i13.ForumPostView,
-    ),
-    _i1.RouteDef(
-      Routes.forumLoginView,
-      page: _i14.ForumLoginView,
-    ),
-    _i1.RouteDef(
-      Routes.forumUserView,
-      page: _i15.ForumUserView,
-    ),
-    _i1.RouteDef(
       Routes.forumSettingsView,
-      page: _i16.ForumSettingsView,
-    ),
-    _i1.RouteDef(
-      Routes.forumUserProfileView,
-      page: _i17.ForumUserProfileView,
+      page: _i11.ForumSettingsView,
     ),
     _i1.RouteDef(
       Routes.codeRadioView,
-      page: _i18.CodeRadioView,
+      page: _i12.CodeRadioView,
     ),
     _i1.RouteDef(
       Routes.superBlockView,
-      page: _i19.SuperBlockView,
+      page: _i13.SuperBlockView,
     ),
     _i1.RouteDef(
       Routes.challengeView,
-      page: _i20.ChallengeView,
+      page: _i14.ChallengeView,
     ),
     _i1.RouteDef(
       Routes.profileView,
-      page: _i21.ProfileView,
+      page: _i15.ProfileView,
     ),
     _i1.RouteDef(
       Routes.webViewView,
-      page: _i22.WebViewView,
+      page: _i16.WebViewView,
     ),
     _i1.RouteDef(
       Routes.learnView,
-      page: _i23.LearnView,
+      page: _i17.LearnView,
     ),
   ];
 
@@ -292,81 +238,32 @@ class StackedRouter extends _i1.RouterBase {
         settings: data,
       );
     },
-    _i11.ForumCategoryView: (data) {
-      final args = data.getArgs<ForumCategoryViewArguments>(
-        orElse: () => const ForumCategoryViewArguments(),
-      );
+    _i11.ForumSettingsView: (data) {
       return MaterialPageRoute<dynamic>(
-        builder: (context) => _i11.ForumCategoryView(key: args.key),
+        builder: (context) => const _i11.ForumSettingsView(),
         settings: data,
       );
     },
-    _i12.ForumPostFeedView: (data) {
-      final args = data.getArgs<ForumPostFeedViewArguments>(nullOk: false);
+    _i12.CodeRadioView: (data) {
       return MaterialPageRoute<dynamic>(
-        builder: (context) => _i12.ForumPostFeedView(
-            key: args.key, slug: args.slug, id: args.id, name: args.name),
+        builder: (context) => const _i12.CodeRadioView(),
         settings: data,
       );
     },
-    _i13.ForumPostView: (data) {
-      final args = data.getArgs<ForumPostViewArguments>(nullOk: false);
-      return MaterialPageRoute<dynamic>(
-        builder: (context) =>
-            _i13.ForumPostView(key: args.key, id: args.id, slug: args.slug),
-        settings: data,
-      );
-    },
-    _i14.ForumLoginView: (data) {
-      final args = data.getArgs<ForumLoginViewArguments>(
-        orElse: () => const ForumLoginViewArguments(),
-      );
-      return MaterialPageRoute<dynamic>(
-        builder: (context) => _i14.ForumLoginView(
-            key: args.key, fromCreatePost: args.fromCreatePost),
-        settings: data,
-      );
-    },
-    _i15.ForumUserView: (data) {
-      final args = data.getArgs<ForumUserViewArguments>(nullOk: false);
-      return MaterialPageRoute<dynamic>(
-        builder: (context) =>
-            _i15.ForumUserView(key: args.key, username: args.username),
-        settings: data,
-      );
-    },
-    _i16.ForumSettingsView: (data) {
-      return MaterialPageRoute<dynamic>(
-        builder: (context) => const _i16.ForumSettingsView(),
-        settings: data,
-      );
-    },
-    _i17.ForumUserProfileView: (data) {
-      return MaterialPageRoute<dynamic>(
-        builder: (context) => const _i17.ForumUserProfileView(),
-        settings: data,
-      );
-    },
-    _i18.CodeRadioView: (data) {
-      return MaterialPageRoute<dynamic>(
-        builder: (context) => const _i18.CodeRadioView(),
-        settings: data,
-      );
-    },
-    _i19.SuperBlockView: (data) {
+    _i13.SuperBlockView: (data) {
       final args = data.getArgs<SuperBlockViewArguments>(nullOk: false);
       return MaterialPageRoute<dynamic>(
-        builder: (context) => _i19.SuperBlockView(
+        builder: (context) => _i13.SuperBlockView(
             key: args.key,
             superBlockDashedName: args.superBlockDashedName,
             superblockName: args.superblockName),
         settings: data,
       );
     },
-    _i20.ChallengeView: (data) {
+    _i14.ChallengeView: (data) {
       final args = data.getArgs<ChallengeViewArguments>(nullOk: false);
       return MaterialPageRoute<dynamic>(
-        builder: (context) => _i20.ChallengeView(
+        builder: (context) => _i14.ChallengeView(
             key: args.key,
             url: args.url,
             block: args.block,
@@ -374,22 +271,22 @@ class StackedRouter extends _i1.RouterBase {
         settings: data,
       );
     },
-    _i21.ProfileView: (data) {
+    _i15.ProfileView: (data) {
       return MaterialPageRoute<dynamic>(
-        builder: (context) => const _i21.ProfileView(),
+        builder: (context) => const _i15.ProfileView(),
         settings: data,
       );
     },
-    _i22.WebViewView: (data) {
+    _i16.WebViewView: (data) {
       final args = data.getArgs<WebViewViewArguments>(nullOk: false);
       return MaterialPageRoute<dynamic>(
-        builder: (context) => _i22.WebViewView(key: args.key, url: args.url),
+        builder: (context) => _i16.WebViewView(key: args.key, url: args.url),
         settings: data,
       );
     },
-    _i23.LearnView: (data) {
+    _i17.LearnView: (data) {
       return MaterialPageRoute<dynamic>(
-        builder: (context) => const _i23.LearnView(),
+        builder: (context) => const _i17.LearnView(),
         settings: data,
       );
     },
@@ -408,11 +305,11 @@ class EpisodeViewArguments {
     required this.podcast,
   });
 
-  final _i24.Key? key;
+  final _i18.Key? key;
 
-  final _i25.Episodes episode;
+  final _i19.Episodes episode;
 
-  final _i26.Podcasts podcast;
+  final _i20.Podcasts podcast;
 }
 
 class NewsTutorialViewArguments {
@@ -421,7 +318,7 @@ class NewsTutorialViewArguments {
     required this.refId,
   });
 
-  final _i24.Key? key;
+  final _i18.Key? key;
 
   final String refId;
 }
@@ -432,9 +329,9 @@ class NewsBookmarkPostViewArguments {
     required this.tutorial,
   });
 
-  final _i24.Key? key;
+  final _i18.Key? key;
 
-  final _i27.BookmarkedTutorial tutorial;
+  final _i21.BookmarkedTutorial tutorial;
 }
 
 class NewsFeedViewArguments {
@@ -449,7 +346,7 @@ class NewsFeedViewArguments {
     this.subject = '',
   });
 
-  final _i24.Key? key;
+  final _i18.Key? key;
 
   final String slug;
 
@@ -472,7 +369,7 @@ class NewsAuthorViewArguments {
     required this.authorSlug,
   });
 
-  final _i24.Key? key;
+  final _i18.Key? key;
 
   final String authorSlug;
 }
@@ -483,68 +380,9 @@ class NewsImageViewArguments {
     required this.imgUrl,
   });
 
-  final _i24.Key? key;
+  final _i18.Key? key;
 
   final String imgUrl;
-}
-
-class ForumCategoryViewArguments {
-  const ForumCategoryViewArguments({this.key});
-
-  final _i24.Key? key;
-}
-
-class ForumPostFeedViewArguments {
-  const ForumPostFeedViewArguments({
-    this.key,
-    required this.slug,
-    required this.id,
-    required this.name,
-  });
-
-  final _i24.Key? key;
-
-  final String slug;
-
-  final String id;
-
-  final String name;
-}
-
-class ForumPostViewArguments {
-  const ForumPostViewArguments({
-    this.key,
-    required this.id,
-    required this.slug,
-  });
-
-  final _i24.Key? key;
-
-  final String id;
-
-  final String slug;
-}
-
-class ForumLoginViewArguments {
-  const ForumLoginViewArguments({
-    this.key,
-    this.fromCreatePost = false,
-  });
-
-  final _i24.Key? key;
-
-  final bool fromCreatePost;
-}
-
-class ForumUserViewArguments {
-  const ForumUserViewArguments({
-    this.key,
-    required this.username,
-  });
-
-  final _i24.Key? key;
-
-  final String username;
 }
 
 class SuperBlockViewArguments {
@@ -554,7 +392,7 @@ class SuperBlockViewArguments {
     required this.superblockName,
   });
 
-  final _i24.Key? key;
+  final _i18.Key? key;
 
   final String superBlockDashedName;
 
@@ -569,11 +407,11 @@ class ChallengeViewArguments {
     required this.challengesCompleted,
   });
 
-  final _i24.Key? key;
+  final _i18.Key? key;
 
   final String url;
 
-  final _i28.Block block;
+  final _i22.Block block;
 
   final int challengesCompleted;
 }
@@ -584,12 +422,12 @@ class WebViewViewArguments {
     required this.url,
   });
 
-  final _i24.Key? key;
+  final _i18.Key? key;
 
   final String url;
 }
 
-extension NavigatorStateExtension on _i29.NavigationService {
+extension NavigatorStateExtension on _i23.NavigationService {
   Future<dynamic> navigateToHomeView([
     int? routerId,
     bool preventDuplicates = true,
@@ -633,9 +471,9 @@ extension NavigatorStateExtension on _i29.NavigationService {
   }
 
   Future<dynamic> navigateToEpisodeView({
-    _i24.Key? key,
-    required _i25.Episodes episode,
-    required _i26.Podcasts podcast,
+    _i18.Key? key,
+    required _i19.Episodes episode,
+    required _i20.Podcasts podcast,
     int? routerId,
     bool preventDuplicates = true,
     Map<String, String>? parameters,
@@ -652,7 +490,7 @@ extension NavigatorStateExtension on _i29.NavigationService {
   }
 
   Future<dynamic> navigateToNewsTutorialView({
-    _i24.Key? key,
+    _i18.Key? key,
     required String refId,
     int? routerId,
     bool preventDuplicates = true,
@@ -669,8 +507,8 @@ extension NavigatorStateExtension on _i29.NavigationService {
   }
 
   Future<dynamic> navigateToNewsBookmarkPostView({
-    _i24.Key? key,
-    required _i27.BookmarkedTutorial tutorial,
+    _i18.Key? key,
+    required _i21.BookmarkedTutorial tutorial,
     int? routerId,
     bool preventDuplicates = true,
     Map<String, String>? parameters,
@@ -686,7 +524,7 @@ extension NavigatorStateExtension on _i29.NavigationService {
   }
 
   Future<dynamic> navigateToNewsFeedView({
-    _i24.Key? key,
+    _i18.Key? key,
     String slug = '',
     String author = '',
     bool fromAuthor = false,
@@ -717,7 +555,7 @@ extension NavigatorStateExtension on _i29.NavigationService {
   }
 
   Future<dynamic> navigateToNewsAuthorView({
-    _i24.Key? key,
+    _i18.Key? key,
     required String authorSlug,
     int? routerId,
     bool preventDuplicates = true,
@@ -734,7 +572,7 @@ extension NavigatorStateExtension on _i29.NavigationService {
   }
 
   Future<dynamic> navigateToNewsImageView({
-    _i24.Key? key,
+    _i18.Key? key,
     required String imgUrl,
     int? routerId,
     bool preventDuplicates = true,
@@ -750,95 +588,6 @@ extension NavigatorStateExtension on _i29.NavigationService {
         transition: transition);
   }
 
-  Future<dynamic> navigateToForumCategoryView({
-    _i24.Key? key,
-    int? routerId,
-    bool preventDuplicates = true,
-    Map<String, String>? parameters,
-    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
-        transition,
-  }) async {
-    return navigateTo<dynamic>(Routes.forumCategoryView,
-        arguments: ForumCategoryViewArguments(key: key),
-        id: routerId,
-        preventDuplicates: preventDuplicates,
-        parameters: parameters,
-        transition: transition);
-  }
-
-  Future<dynamic> navigateToForumPostFeedView({
-    _i24.Key? key,
-    required String slug,
-    required String id,
-    required String name,
-    int? routerId,
-    bool preventDuplicates = true,
-    Map<String, String>? parameters,
-    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
-        transition,
-  }) async {
-    return navigateTo<dynamic>(Routes.forumPostFeedView,
-        arguments: ForumPostFeedViewArguments(
-            key: key, slug: slug, id: id, name: name),
-        id: routerId,
-        preventDuplicates: preventDuplicates,
-        parameters: parameters,
-        transition: transition);
-  }
-
-  Future<dynamic> navigateToForumPostView({
-    _i24.Key? key,
-    required String id,
-    required String slug,
-    int? routerId,
-    bool preventDuplicates = true,
-    Map<String, String>? parameters,
-    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
-        transition,
-  }) async {
-    return navigateTo<dynamic>(Routes.forumPostView,
-        arguments: ForumPostViewArguments(key: key, id: id, slug: slug),
-        id: routerId,
-        preventDuplicates: preventDuplicates,
-        parameters: parameters,
-        transition: transition);
-  }
-
-  Future<dynamic> navigateToForumLoginView({
-    _i24.Key? key,
-    bool fromCreatePost = false,
-    int? routerId,
-    bool preventDuplicates = true,
-    Map<String, String>? parameters,
-    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
-        transition,
-  }) async {
-    return navigateTo<dynamic>(Routes.forumLoginView,
-        arguments:
-            ForumLoginViewArguments(key: key, fromCreatePost: fromCreatePost),
-        id: routerId,
-        preventDuplicates: preventDuplicates,
-        parameters: parameters,
-        transition: transition);
-  }
-
-  Future<dynamic> navigateToForumUserView({
-    _i24.Key? key,
-    required String username,
-    int? routerId,
-    bool preventDuplicates = true,
-    Map<String, String>? parameters,
-    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
-        transition,
-  }) async {
-    return navigateTo<dynamic>(Routes.forumUserView,
-        arguments: ForumUserViewArguments(key: key, username: username),
-        id: routerId,
-        preventDuplicates: preventDuplicates,
-        parameters: parameters,
-        transition: transition);
-  }
-
   Future<dynamic> navigateToForumSettingsView([
     int? routerId,
     bool preventDuplicates = true,
@@ -847,20 +596,6 @@ extension NavigatorStateExtension on _i29.NavigationService {
         transition,
   ]) async {
     return navigateTo<dynamic>(Routes.forumSettingsView,
-        id: routerId,
-        preventDuplicates: preventDuplicates,
-        parameters: parameters,
-        transition: transition);
-  }
-
-  Future<dynamic> navigateToForumUserProfileView([
-    int? routerId,
-    bool preventDuplicates = true,
-    Map<String, String>? parameters,
-    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
-        transition,
-  ]) async {
-    return navigateTo<dynamic>(Routes.forumUserProfileView,
         id: routerId,
         preventDuplicates: preventDuplicates,
         parameters: parameters,
@@ -882,7 +617,7 @@ extension NavigatorStateExtension on _i29.NavigationService {
   }
 
   Future<dynamic> navigateToSuperBlockView({
-    _i24.Key? key,
+    _i18.Key? key,
     required String superBlockDashedName,
     required String superblockName,
     int? routerId,
@@ -903,9 +638,9 @@ extension NavigatorStateExtension on _i29.NavigationService {
   }
 
   Future<dynamic> navigateToChallengeView({
-    _i24.Key? key,
+    _i18.Key? key,
     required String url,
-    required _i28.Block block,
+    required _i22.Block block,
     required int challengesCompleted,
     int? routerId,
     bool preventDuplicates = true,
@@ -940,7 +675,7 @@ extension NavigatorStateExtension on _i29.NavigationService {
   }
 
   Future<dynamic> navigateToWebViewView({
-    _i24.Key? key,
+    _i18.Key? key,
     required String url,
     int? routerId,
     bool preventDuplicates = true,
