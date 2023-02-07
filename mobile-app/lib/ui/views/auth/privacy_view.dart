233f41a5d53ef1b6579e42f66bc9097150d05d3b
Niraj Nandish <nirajnandish@icloud.com>
2023-02-08 01:15:00 +0530

    feat: store paragraph text style in variable
    Co-authored-by: Sem Bauke <semboot699@gmail.com>

--- mobile-app/lib/ui/views/auth/privacy_view.dart
+++ mobile-app/lib/ui/views/auth/privacy_view.dart
@@ -6,6 +6,11 @@ class PrivacyView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const paragraphTextStyle = TextStyle(
+      fontSize: 18,
+      fontFamily: 'Lato',
+      height: 1.2,
+    );
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -30,132 +35,84 @@ class PrivacyView extends StatelessWidget {
               ),
               const Text(
                 'freeCodeCamp is a proven path to your first software developer job.',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontFamily: 'Lato',
-                  height: 1.2,
-                ),
+                style: paragraphTextStyle,
               ),
               const SizedBox(
                 height: 8,
               ),
               const Text(
                 'More than 40,000 people have gotten developer jobs after completing this — including at big companies like Google and Microsoft.',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontFamily: 'Lato',
-                  height: 1.2,
-                ),
+                style: paragraphTextStyle,
               ),
               const SizedBox(
                 height: 8,
               ),
               const Text(
                 'More than 40,000 people have gotten developer jobs after completing this - including at big companies like Google and Microsoft.',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontFamily: 'Lato',
-                  height: 1.2,
-                ),
+                style: paragraphTextStyle,
               ),
               const SizedBox(
                 height: 8,
               ),
               const Text(
                 'If you are new to programming, we recommend you start at the beginning and earn these certifications in order.',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontFamily: 'Lato',
-                  height: 1.2,
-                ),
+                style: paragraphTextStyle,
               ),
               const SizedBox(
                 height: 8,
               ),
               const Text(
                 'To earn each certification, build its 5 required projects and get all their tests to pass.',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontFamily: 'Lato',
-                  height: 1.2,
-                ),
+                style: paragraphTextStyle,
               ),
               const SizedBox(
                 height: 8,
               ),
               const Text(
                 'You can add these certifications to your résumé or LinkedIn. But more important than the certifications is the practice you get along the way.',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontFamily: 'Lato',
-                  height: 1.2,
-                ),
+                style: paragraphTextStyle,
               ),
               const SizedBox(
                 height: 8,
               ),
               const Text(
                 'If you feel overwhelmed, that is normal. Programming is hard.',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontFamily: 'Lato',
-                  height: 1.2,
-                ),
+                style: paragraphTextStyle,
               ),
               const SizedBox(
                 height: 8,
               ),
               const Text(
                 'Practice is the key. Practice, practice, practice.',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontFamily: 'Lato',
-                  height: 1.2,
-                ),
+                style: paragraphTextStyle,
               ),
               const SizedBox(
                 height: 8,
               ),
               const Text(
                 'And this curriculum will give you thousands of hours of hands-on programming practice.',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontFamily: 'Lato',
-                  height: 1.2,
-                ),
+                style: paragraphTextStyle,
               ),
               const SizedBox(
                 height: 8,
               ),
               const Text(
                 'And if you want to learn more math and computer science theory, we also have thousands of hours of video courses on freeCodeCamp\'s YouTube channel.',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontFamily: 'Lato',
-                  height: 1.2,
-                ),
+                style: paragraphTextStyle,
               ),
               const SizedBox(
                 height: 8,
               ),
               const Text(
                 'If you want to get a developer job or freelance clients, programming skills will be just part of the puzzle. You also need to build your personal network and your reputation as a developer.',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontFamily: 'Lato',
-                  height: 1.2,
-                ),
+                style: paragraphTextStyle,
               ),
               const SizedBox(
                 height: 8,
               ),
               const Text(
                 'You can do this on LinkedIn and GitHub, and also on the freeCodeCamp forum.',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontFamily: 'Lato',
-                  height: 1.2,
-                ),
+                style: paragraphTextStyle,
               ),
               const SizedBox(
                 height: 8,
@@ -188,11 +145,7 @@ class PrivacyView extends StatelessWidget {
               ),
               const Text(
                 'By the way, each Friday I send an email with 5 links about programming and computer science. I send these to about 4 million people. Would you like me to send this to you, too?',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontFamily: 'Lato',
-                  height: 1.2,
-                ),
+                style: paragraphTextStyle,
               ),
               const SizedBox(
                 height: 12,
