e750ee468f758014fbaa5fb8228138c21e906a3c
Niraj Nandish <nirajnandish@icloud.com>
2023-04-11 18:46:28 +0530

    feat: iOS deployment (#885)
    * feat: disable firebase tools for tests
    
    * feat: update firebase config for ios
    
    * fix: temp fix for e2e test for iOS
    
    * chore: update Ci/CD workflows for
    
    * feat: ios deployment config
    
    * feat: auth0 config
    
    * chore: ci screenshot zip name

--- mobile-app/integration_test/news_test.dart
+++ mobile-app/integration_test/news_test.dart
@@ -15,7 +15,7 @@ void main() {
     testWidgets('should bookmark tutorial', (WidgetTester tester) async {
       // Start app
       tester.printToConsole('Test starting');
-      await app.main();
+      await app.main(testing: true);
       await binding.convertFlutterSurfaceToImage();
       await tester.pumpAndSettle();
       await binding.takeScreenshot('news-feed');
