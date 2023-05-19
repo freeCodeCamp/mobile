f4fa10023275d0ecd822594c0c6a542fad1d2a59
sembauke <semboot699@gmail.com>
2023-05-19 11:52:15 +0200

    feat: add are you sure? Dialog

--- mobile-app/lib/ui/views/settings/settings_view.dart
+++ mobile-app/lib/ui/views/settings/settings_view.dart
@@ -10,6 +10,7 @@ class SettingsView extends StatelessWidget {
   Widget build(BuildContext context) {
     return ViewModelBuilder<SettingsViewModel>.reactive(
       viewModelBuilder: () => SettingsViewModel(),
+      onViewModelReady: (viewModel) => viewModel.init(),
       builder: (context, model, child) => Scaffold(
         appBar: AppBar(
           title: const Text('SETTINGS'),
--- mobile-app/lib/ui/views/settings/settings_viewmodel.dart
+++ mobile-app/lib/ui/views/settings/settings_viewmodel.dart
@@ -1,11 +1,31 @@
 import 'package:flutter_custom_tabs/flutter_custom_tabs.dart';
+import 'package:freecodecamp/app/app.locator.dart';
+import 'package:freecodecamp/enums/dialog_type.dart';
+import 'package:freecodecamp/ui/widgets/setup_dialog_ui.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
 
 class SettingsViewModel extends BaseViewModel {
+  final DialogService _dialogService = locator<DialogService>();
+
+  void init() {
+    setupDialogUi();
+  }
+
   void resetCache() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.clear();
+    DialogResponse? res = await _dialogService.showCustomDialog(
+      variant: DialogType.buttonForm,
+      title: 'Clear Cache',
+      description:
+          'Are you sure you want to clear the cache? - this resets all your progress and local data',
+      mainButtonTitle: 'Clear',
+    );
+
+    if (res?.confirmed == true) {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      prefs.clear();
+    }
   }
 
   void openPrivacyPolicy() {
