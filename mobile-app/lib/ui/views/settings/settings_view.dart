549bfd655bd0124f3b5047000838c984ca1394fa
sembauke <semboot699@gmail.com>
2023-05-19 11:39:49 +0200

    feat: add clear cache button

--- mobile-app/lib/ui/views/settings/settings_view.dart
+++ mobile-app/lib/ui/views/settings/settings_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:freecodecamp/ui/views/settings/settings_viewmodel.dart';
+import 'package:freecodecamp/ui/widgets/drawer_widget/drawer_widget_view.dart';
 import 'package:stacked/stacked.dart';
 
 class SettingsView extends StatelessWidget {
@@ -7,14 +8,29 @@ class SettingsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ViewModelBuilder.reactive(
+    return ViewModelBuilder<SettingsViewModel>.reactive(
       viewModelBuilder: () => SettingsViewModel(),
       builder: (context, model, child) => Scaffold(
         appBar: AppBar(
-          title: const Text('SETTIGNS'),
+          title: const Text('SETTINGS'),
         ),
-        body: const Center(
-          child: Text('SettingsView'),
+        body: Column(
+          children: [
+            ListTile(
+              leading: const Icon(Icons.dataset_linked),
+              title: const Text('Reset Cache'),
+              subtitle: const Text('Clears all local data and progress'),
+              onTap: () => model.resetCache(),
+            ),
+            buildDivider(),
+            ListTile(
+              leading: const Icon(Icons.privacy_tip),
+              title: const Text('Privacy Policy'),
+              subtitle: const Text('Read our privacy policy'),
+              onTap: () => model.openPrivacyPolicy(),
+            ),
+            buildDivider(),
+          ],
         ),
       ),
     );
--- mobile-app/lib/ui/views/settings/settings_viewmodel.dart
+++ mobile-app/lib/ui/views/settings/settings_viewmodel.dart
@@ -1,3 +1,25 @@
+import 'package:flutter_custom_tabs/flutter_custom_tabs.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:stacked/stacked.dart';
 
-class SettingsViewModel extends BaseViewModel {}
+class SettingsViewModel extends BaseViewModel {
+  void resetCache() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.clear();
+  }
+
+  void openPrivacyPolicy() {
+    launch(
+      'https://www.freecodecamp.org/news/privacy-policy/',
+      customTabsOption: const CustomTabsOption(
+        enableDefaultShare: true,
+        enableUrlBarHiding: true,
+        showPageTitle: true,
+        extraCustomTabs: [
+          'org.mozilla.firefox',
+          'com.microsoft.emmx',
+        ],
+      ),
+    );
+  }
+}
