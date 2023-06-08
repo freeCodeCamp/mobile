19296b679fff937b1f5800600f95ef081af383e7
sembauke <semboot699@gmail.com>
2023-06-08 16:48:41 +0200

    fix: scroll physicis cosnole

--- mobile-app/lib/ui/views/learn/widgets/console/console_view.dart
+++ mobile-app/lib/ui/views/learn/widgets/console/console_view.dart
@@ -26,6 +26,8 @@ class JavaScriptConsole extends StatelessWidget {
               Expanded(
                 child: Scrollbar(
                   child: ListView.builder(
+                    padding: const EdgeInsets.all(8),
+                    physics: const ClampingScrollPhysics(),
                     itemCount: messages.isEmpty ? 1 : messages.length,
                     itemBuilder: (context, index) {
                       List<Widget> htmlWidgets = HtmlHandler.htmlHandler(
