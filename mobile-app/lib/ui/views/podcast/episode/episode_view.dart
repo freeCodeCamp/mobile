4b3e0337a5d11d29810cf68478cc9d2adc9137a8
Sem Bauke <semboot699@gmail.com>
2023-06-12 19:11:41 +0200

    feat: upgrade flutter_html package and restructure parser (#958)
    * feat: dry html parser
    
    * fix: code parsing
    
    * feat: flutter html 3.0.0.beta-1
    
    * fix: extensions
    
    * fix: podcast description widget
    
    * fix: font sizes
    
    * fix: episode list view html
    
    * fix: remove import
    
    * fix: data url images
    
    * fix: italix and link text size
    
    * fix: codeblock position
    
    * fix: youtube iframe width and player controls
    
    * fix: bold text font size
    
    * fix: consistent line spacing in list text
    
    * fix: center align figure caption text
    
    * fix: missing route name
    
    * fix: code block width for non-overflowing code
    
    * fix: podcast description font size
    
    * fix: remove highlight view
    
    * feat: give everything the same font size
    
    * fix: ios podfile dependencies
    
    * fix: set font size to xlarge
    
    * fix: h4 non-relative font size
    
    * fix: text color on font sizes
    
    * fix: make bold text bold again
    
    * fix: text size tables
    
    * fix: remove list wrapper
    
    * fix: block quote layout
    
    ---------
    
    Co-authored-by: Niraj Nandish <nirajnandish@icloud.com>

--- mobile-app/lib/ui/views/podcast/episode/episode_view.dart
+++ mobile-app/lib/ui/views/podcast/episode/episode_view.dart
@@ -92,8 +92,10 @@ class EpisodeView extends StatelessWidget {
   }
 
   Widget description(EpisodeViewModel model, BuildContext context) {
+    HTMLParser parser = HTMLParser(context: context);
     return Column(
-      children: [HtmlHandler.htmlWidgetBuilder(episode.description, context)],
+      children: parser.parse(episode.description ?? ''),
+
     );
   }
 }
