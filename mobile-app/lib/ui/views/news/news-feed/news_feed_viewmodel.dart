9c2a4a7252364f9f1c76d1376dd923c7cc607fca
Niraj Nandish <nirajnandish@icloud.com>
2023-07-26 15:45:57 +0400

    failing test - trial 8

--- mobile-app/lib/ui/views/news/news-feed/news_feed_viewmodel.dart
+++ mobile-app/lib/ui/views/news/news-feed/news_feed_viewmodel.dart
@@ -97,7 +97,7 @@ class NewsFeedViewModel extends BaseViewModel {
       Uri.parse(url),
       headers: {
         'Cache-Control': 'no-cache',
-        'User-Agent': FkUserAgent.userAgent ?? 'Unknown',
+        'User-Agent': FkUserAgent.userAgent ?? 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
       },
     ).timeout(const Duration(minutes: 3));
     stopWatch.stop();
