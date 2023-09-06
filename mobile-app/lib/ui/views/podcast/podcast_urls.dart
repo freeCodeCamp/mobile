2a3beac62a813c125cacf0ea6ed601a5d4a8c9c5
Niraj Nandish <nirajnandish@icloud.com>
2023-09-06 10:42:14 +0400

    chore: remove unused file

--- mobile-app/lib/ui/views/podcast/podcast_urls.dart
+++
@@ -1,6 +0,0 @@
-const podcastUrls = [
-  'https://feed.syntax.fm/rss',
-  'https://changelog.com/podcast/feed',
-  'https://pinecast.com/feed/ladybug-podcast',
-  'https://freecodecamp.libsyn.com/rss'
-];
