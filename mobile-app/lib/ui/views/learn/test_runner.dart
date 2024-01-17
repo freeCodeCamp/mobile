486b23e876c97f226ce27c6119932f88c8ff448f
sembauke <semboot699@gmail.com>
2024-01-17 12:13:29 +0100

    chore: update curriculum helpers

--- mobile-app/lib/ui/views/learn/test_runner.dart
+++ mobile-app/lib/ui/views/learn/test_runner.dart
@@ -191,7 +191,7 @@ class TestRunner extends BaseViewModel {
       String tail = challenge.files[0].tail ?? '';
 
       return '''<script type="module">
-    import * as __helpers from "https://unpkg.com/@freecodecamp/curriculum-helpers@1.2.0/dist/index.js";
+    import * as __helpers from "https://unpkg.com/@freecodecamp/curriculum-helpers@2.0.1/dist/index.js";
 
     const code = `$code`;
     const doc = new DOMParser().parseFromString(code, 'text/html');
