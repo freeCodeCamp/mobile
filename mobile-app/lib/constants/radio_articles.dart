7816c8bf73f0522474e9ceffcb9720e1e2be5eae
Niraj Nandish <nirajnandish@icloud.com>
2024-04-30 17:49:22 +0530

    feat: migrate to official algolia package (#1228)
    * chore: algolia helper package
    
    * chore: move radio article list to constants
    
    * feat: use algolia helpers to fetch articles
    
    * chore: remove algolia package

---
+++ mobile-app/lib/constants/radio_articles.dart
@@ -0,0 +1,9 @@
+List<String> radioArticles = [
+  '622c7bf563ded806bb9e8a20',
+  '5f9ca12d740569d1a4ca4d23',
+  '5f9ca15f740569d1a4ca4e36',
+  '5f9ca198740569d1a4ca4f89',
+  '5f9ca587740569d1a4ca6a0b',
+  '5f9ca99d740569d1a4ca85c3',
+  '5f9cafb9740569d1a4caaf5b'
+];
--- mobile-app/lib/ui/views/news/news-search/news_search_view.dart
+++ mobile-app/lib/ui/views/news/news-search/news_search_view.dart
@@ -1,4 +1,4 @@
-import 'package:algolia/algolia.dart';
+import 'package:algolia_helper_flutter/algolia_helper_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:freecodecamp/extensions/i18n_extension.dart';
 import 'package:freecodecamp/ui/views/news/news-search/news_search_viewmodel.dart';
@@ -6,123 +6,132 @@ import 'package:stacked/stacked.dart';
 
 class NewsSearchView extends StatelessWidget {
   const NewsSearchView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<NewsSearchModel>.reactive(
       viewModelBuilder: () => NewsSearchModel(),
-      builder: (context, model, child) => Scaffold(
-        appBar: AppBar(
-          titleSpacing: 0,
-          title: TextField(
-              controller: model.searchbarController,
-              decoration: InputDecoration(
-                hintText: context.t.tutorial_search_placeholder,
-                fillColor: const Color.fromRGBO(0x2A, 0x2A, 0x40, 1),
-                border: InputBorder.none,
-                filled: true,
-              ),
-              onChanged: (value) {
-                model.setSearchTerm(value);
+      onViewModelReady: (model) => model.init(),
+      onDispose: (model) => model.onDispose(),
+      builder: (context, model, child) {
+        final isSearchable = model.searchbarController.text != '' &&
+            !model.isLoading &&
+            model.hasData;
 
-                if (model.hasData) {
+        return Scaffold(
+          appBar: AppBar(
+            titleSpacing: 0,
+            title: TextField(
+                controller: model.searchbarController,
+                decoration: InputDecoration(
+                  hintText: context.t.tutorial_search_placeholder,
+                  fillColor: const Color.fromRGBO(0x2A, 0x2A, 0x40, 1),
+                  border: InputBorder.none,
+                  filled: true,
+                ),
+                onChanged: (value) {
+                  model.setSearchTerm(value);
                   model.setHasData = false;
-                }
-              },
-              onSubmitted: model.hasData
-                  ? (value) {
-                      model.searchSubject();
-                    }
-                  : (value) {}),
-          actions: [
-            Container(
-              margin: const EdgeInsets.only(left: 8),
-              color: const Color.fromRGBO(0x2A, 0x2A, 0x40, 1),
-              child: IconButton(
-                onPressed: model.searchbarController.text != '' && model.hasData
-                    ? () {
+                  model.setIsLoading = true;
+                },
+                onSubmitted: isSearchable
+                    ? (value) {
                         model.searchSubject();
                       }
-                    : null,
-                icon: const Icon(
-                  Icons.search_sharp,
+                    : (value) {}),
+            actions: [
+              Container(
+                margin: const EdgeInsets.only(left: 8),
+                color: const Color.fromRGBO(0x2A, 0x2A, 0x40, 1),
+                child: IconButton(
+                  onPressed: isSearchable
+                      ? () {
+                          model.searchSubject();
+                        }
+                      : null,
+                  icon: const Icon(
+                    Icons.search_sharp,
+                  ),
                 ),
-              ),
-            )
-          ],
-        ),
-        body: StreamBuilder<List<AlgoliaObjectSnapshot>>(
-          stream: Stream.fromFuture(model.search(model.getSearchTerm)),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
+              )
+            ],
+          ),
+          body: StreamBuilder<SearchResponse>(
+            stream: model.algolia.responses,
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return const Center(
+                  child: Text(
+                    'There was an error loading tutorials \n please try again',
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              }
 
-            if (!snapshot.hasData) {
-              return Center(
-                child: Text(
-                  context.t.tutorial_search_no_results,
-                ),
-              );
-            }
+              if (snapshot.connectionState == ConnectionState.waiting ||
+                  model.isLoading) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
 
-            if (snapshot.hasError) {
-              return const Center(
-                child: Text(
-                  'There was an error loading tutorials \n please try again',
-                  textAlign: TextAlign.center,
-                ),
-              );
-            }
+              if (!snapshot.hasData) {
+                return Center(
+                  child: Text(
+                    context.t.tutorial_search_no_results,
+                  ),
+                );
+              }
 
-            List<AlgoliaObjectSnapshot>? current = snapshot.data;
+              final currentData = snapshot.data!.hits;
 
-            return Column(
-              children: [
-                Expanded(
-                  child: current!.isNotEmpty
-                      ? Scrollbar(
-                          thumbVisibility: true,
-                          trackVisibility: true,
-                          child: ListView.separated(
-                            physics: const ClampingScrollPhysics(),
-                            itemCount: current.length,
-                            separatorBuilder: (context, int i) => const Divider(
-                              color: Color.fromRGBO(0x42, 0x42, 0x55, 1),
-                              thickness: 2,
-                              height: 5,
-                            ),
-                            itemBuilder: (context, index) {
-                              return ListTile(
-                                title: Text(
-                                  current[index].data['title'],
-                                  maxLines: 2,
-                                  overflow: TextOverflow.ellipsis,
-                                  style: TextStyle(
-                                    color: Colors.white.withOpacity(0.87),
+              return Column(
+                children: [
+                  Expanded(
+                    child: currentData.isNotEmpty
+                        ? Scrollbar(
+                            thumbVisibility: true,
+                            trackVisibility: true,
+                            child: ListView.separated(
+                              physics: const ClampingScrollPhysics(),
+                              itemCount: currentData.length,
+                              separatorBuilder: (context, int i) =>
+                                  const Divider(
+                                color: Color.fromRGBO(0x42, 0x42, 0x55, 1),
+                                thickness: 2,
+                                height: 5,
+                              ),
+                              itemBuilder: (context, index) {
+                                return ListTile(
+                                  title: Text(
+                                    currentData[index]['title'],
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(
+                                      color: Colors.white.withOpacity(0.87),
+                                    ),
                                   ),
-                                ),
-                                onTap: () => {
-                                  model.navigateToTutorial(
-                                      current[index].data['objectID'],
-                                      current[index].data['title']),
-                                },
-                              );
-                            },
+                                  onTap: () => {
+                                    model.navigateToTutorial(
+                                        currentData[index]['objectID'],
+                                        currentData[index]['title']),
+                                  },
+                                );
+                              },
+                            ),
+                          )
+                        : Center(
+                            child: Text(
+                              context.t.tutorial_search_no_results,
+                            ),
                           ),
-                        )
-                      : Center(
-                          child: Text(
-                            context.t.tutorial_search_no_results,
-                          ),
-                        ),
-                ),
-              ],
-            );
-          },
-        ),
-      ),
+                  ),
+                ],
+              );
+            },
+          ),
+        );
+      },
     );
   }
 }
