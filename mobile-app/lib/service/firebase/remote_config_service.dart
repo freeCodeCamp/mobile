634e04456fc4226b4d9117978011f48c18d2e825
Niraj Nandish <nirajnandish@icloud.com>
2024-11-14 00:23:30 +0530

    release(app): v4.4.0+40400

--- mobile-app/lib/service/firebase/remote_config_service.dart
+++ mobile-app/lib/service/firebase/remote_config_service.dart
@@ -24,7 +24,7 @@ class RemoteConfigService {
         ),
       );
       await remoteConfig.setDefaults({
-        'min_app_version': '4.3.0',
+        'min_app_version': '4.4.0',
       });
 
       await remoteConfig.fetchAndActivate();
