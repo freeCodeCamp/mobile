4abc97110a0f610095c9a375b946e5598d7255ca
sembauke <semboot699@gmail.com>
2024-12-23 15:26:30 +0100

    fix: disable browser extensions check in test runner

--- mobile-app/lib/ui/views/learn/test_runner.dart
+++ mobile-app/lib/ui/views/learn/test_runner.dart
@@ -205,6 +205,7 @@ class TestRunner extends BaseViewModel {
     """ : ''}
 
     const assert = chai.assert;
+    const __checkForBrowserExtensions = false;
     const tests = ${parseTest(challenge.tests)};
     const testText = ${challenge.tests.map((e) => '''"${e.instruction.replaceAll('"', '\\"').replaceAll('\n', ' ')}"''').toList().toString()};
 
@@ -254,7 +255,7 @@ class TestRunner extends BaseViewModel {
       import * as __helpers from "https://unpkg.com/@freecodecamp/curriculum-helpers@1.1.0/dist/index.js";
 
       const assert = chai.assert;
-
+      const __checkForBrowserExtensions = false;
       let code = `$code`;
       let head = `$head`;
       let tail = `$tail`;
