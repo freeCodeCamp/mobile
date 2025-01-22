172ce99dae8813b87c584519126a953afe054564
Niraj Nandish <nirajnandish@icloud.com>
2025-01-22 21:06:16 +0530

    fix: use iPhone 16 Pro Max for iOS CI/CD tests

--- mobile-app/integration_test_runner.dart
+++ mobile-app/integration_test_runner.dart
@@ -37,7 +37,7 @@ brew install applesimutils
           'applesimutils',
           [
             '--byName',
-            'iPhone 15 Pro Max',
+            'iPhone 16 Pro Max',
             '--bundle',
             'org.freecodecamp.ios',
             '--setPermissions',
