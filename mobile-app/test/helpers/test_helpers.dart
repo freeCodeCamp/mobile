ca2854afd84f57d53c9a6dda9781d6f1c42eb522
Huyen Nguyen <25715018+huyenltnguyen@users.noreply.github.com>
2025-04-09 22:43:36 +0700

    test: profile view

--- mobile-app/test/helpers/test_helpers.dart
+++ mobile-app/test/helpers/test_helpers.dart
@@ -1,10 +1,11 @@
 import 'package:freecodecamp/app/app.locator.dart';
+import 'package:freecodecamp/service/authentication/authentication_service.dart';
 import 'package:freecodecamp/service/news/bookmark_service.dart';
 import 'package:mockito/annotations.dart';
 import 'package:stacked_services/stacked_services.dart';
 // @stacked-import
 
-import 'test_helpers.mocks.dart';
+import './test_helpers.mocks.dart';
 
 @GenerateMocks([], customMocks: [
   MockSpec<NavigationService>(onMissingStub: OnMissingStub.returnDefault),
@@ -15,6 +16,8 @@ void registerServices() {
   getAndRegisterNavigationService();
   getAndRegisterDialogService();
   getAndRegisterNewsBookmarkService();
+  getAndRegisterAuthenticationService();
+  getAndRegisterSnackbarService();
 // @stacked-mock-register
 }
 
@@ -38,10 +41,32 @@ BookmarksDatabaseService getAndRegisterNewsBookmarkService() {
   locator.registerSingleton<BookmarksDatabaseService>(service);
   return service;
 }
+
+AuthenticationService getAndRegisterAuthenticationService() {
+  _removeRegistrationIfExists<AuthenticationService>();
+  final service = AuthenticationService();
+  locator.registerLazySingleton<AuthenticationService>(() => service);
+  return service;
+}
+
+SnackbarService getAndRegisterSnackbarService() {
+  _removeRegistrationIfExists<SnackbarService>();
+  final service = SnackbarService();
+  locator.registerLazySingleton<SnackbarService>(() => service);
+  return service;
+}
 // @stacked-mock-create
 
 void _removeRegistrationIfExists<T extends Object>() {
   if (locator.isRegistered<T>()) {
     locator.unregister<T>();
   }
+}
+
+void unregisterService() {
+  locator.unregister<MockDialogService>();
+  locator.unregister<MockNavigationService>();
+  locator.unregister<BookmarksDatabaseService>();
+  locator.unregister<AuthenticationService>();
+  locator.unregister<SnackbarService>();
 }
