1d8d76803ef3462d68e88f575d3a6f5def0a3b62
Huyen Nguyen <25715018+huyenltnguyen@users.noreply.github.com>
2025-04-21 17:59:23 +0700

    fix: convert to classes

--- mobile-app/lib/ui/theme/fcc_theme.dart
+++ mobile-app/lib/ui/theme/fcc_theme.dart
@@ -1,81 +1,73 @@
 import 'package:flutter/material.dart';
 
 // Based on https://github.com/freeCodeCamp/ui/blob/01048178d606f404256c8f02299f6323c25e6a73/src/colors.css#L1-L50
-const fccColors = {
-  // Gray
-  'gray00': Color(0xFFFFFFFF),
-  'gray05': Color(0xFFF5F6F7),
-  'gray10': Color(0xFFDFDFE2),
-  'gray15': Color(0xFFD0D0D5),
-  'gray45': Color(0xFF858591),
-  'gray75': Color(0xFF3B3B4F),
-  'gray80': Color(0xFF2A2A40),
-  'gray85': Color(0xFF1B1B32),
-  'gray90': Color(0xFF0A0A23),
+class FccColors {
+  static const gray00 = Color(0xFFFFFFFF);
+  static const gray05 = Color(0xFFF5F6F7);
+  static const gray10 = Color(0xFFDFDFE2);
+  static const gray15 = Color(0xFFD0D0D5);
+  static const gray45 = Color(0xFF858591);
+  static const gray75 = Color(0xFF3B3B4F);
+  static const gray80 = Color(0xFF2A2A40);
+  static const gray85 = Color(0xFF1B1B32);
+  static const gray90 = Color(0xFF0A0A23);
 
-  // Purple
-  'purple10': Color(0xFFDBB8FF),
-  'purple50': Color(0xFF9400D3),
-  'purple90': Color(0xFF5A01A7),
+  static const purple10 = Color(0xFFDBB8FF);
+  static const purple50 = Color(0xFF9400D3);
+  static const purple90 = Color(0xFF5A01A7);
 
-  // Yellow
-  'yellow05': Color(0xFFFCF8E3),
-  'yellow10': Color(0xFFFAEBCC),
-  'yellow40': Color(0xFFFFC300),
-  'yellow45': Color(0xFFFFBF00),
-  'yellow50': Color(0xFFF1BE32),
-  'yellow70': Color(0xFF8A6D3B),
-  'yellow90': Color(0xFF4D3800),
+  static const yellow05 = Color(0xFFFCF8E3);
+  static const yellow10 = Color(0xFFFAEBCC);
+  static const yellow40 = Color(0xFFFFC300);
+  static const yellow45 = Color(0xFFFFBF00);
+  static const yellow50 = Color(0xFFF1BE32);
+  static const yellow70 = Color(0xFF8A6D3B);
+  static const yellow90 = Color(0xFF4D3800);
 
-  // Blue
-  'blue05': Color(0xFFD9EDF7),
-  'blue10': Color(0xFFBCE8F1),
-  'blue30': Color(0xFF99C9FF),
-  'blue50': Color(0xFF198EEE),
-  'blue70': Color(0xFF31708F),
-  'blue90': Color(0xFF002EAD),
-  'blue30Translucent': Color.fromRGBO(153, 201, 255, 0.3),
-  'blue90Translucent': Color.fromRGBO(0, 46, 173, 0.3),
+  static const blue05 = Color(0xFFD9EDF7);
+  static const blue10 = Color(0xFFBCE8F1);
+  static const blue30 = Color(0xFF99C9FF);
+  static const blue50 = Color(0xFF198EEE);
+  static const blue70 = Color(0xFF31708F);
+  static const blue90 = Color(0xFF002EAD);
+  static const blue30Translucent = Color.fromRGBO(153, 201, 255, 0.3);
+  static const blue90Translucent = Color.fromRGBO(0, 46, 173, 0.3);
 
-  // Green
-  'green05': Color(0xFFDFF0D8),
-  'green10': Color(0xFFD6E9C6),
-  'green40': Color(0xFFACD157),
-  'green70': Color(0xFF3C763D),
-  'green90': Color(0xFF00471B),
+  static const green05 = Color(0xFFDFF0D8);
+  static const green10 = Color(0xFFD6E9C6);
+  static const green40 = Color(0xFFACD157);
+  static const green70 = Color(0xFF3C763D);
+  static const green90 = Color(0xFF00471B);
 
-  // Red
-  'red05': Color(0xFFF2DEDE),
-  'red10': Color(0xFFEBCCD1),
-  'red15': Color(0xFFFFADAD),
-  'red30': Color(0xFFF8577C),
-  'red70': Color(0xFFA94442),
-  'red80': Color(0xFFF82153),
-  'red90': Color(0xFF850000),
-};
+  static const red05 = Color(0xFFF2DEDE);
+  static const red10 = Color(0xFFEBCCD1);
+  static const red15 = Color(0xFFFFADAD);
+  static const red30 = Color(0xFFF8577C);
+  static const red70 = Color(0xFFA94442);
+  static const red80 = Color(0xFFF82153);
+  static const red90 = Color(0xFF850000);
+}
 
 // Based on https://github.com/freeCodeCamp/ui/blob/01048178d606f404256c8f02299f6323c25e6a73/src/colors.css#L120-L140
-final fccSemanticColors = {
-  // Foreground
-  'foregroundPrimary': fccColors['gray00'],
-  'foregroundSecondary': fccColors['gray05'],
-  'foregroundTertiary': fccColors['gray10'],
-  'foregroundQuaternary': fccColors['gray15'],
-  'foregroundDanger': fccColors['red90'],
-  'foregroundSuccess': fccColors['green90'],
-  'foregroundInfo': fccColors['blue90'],
-  'foregroundWarning': fccColors['yellow40'],
+class FccSemanticColors {
+  static const foregroundPrimary = FccColors.gray00;
+  static const foregroundSecondary = FccColors.gray05;
+  static const foregroundTertiary = FccColors.gray10;
+  static const foregroundQuaternary = FccColors.gray15;
+  static const foregroundDanger = FccColors.red90;
+  static const foregroundSuccess = FccColors.green90;
+  static const foregroundInfo = FccColors.blue90;
+  static const foregroundWarning = FccColors.yellow40;
 
-  // Background
-  'backgroundPrimary': fccColors['gray90'],
-  'backgroundSecondary': fccColors['gray85'],
-  'backgroundTertiary': fccColors['gray80'],
-  'backgroundQuaternary': fccColors['gray75'],
-  'backgroundDanger': fccColors['red15'],
-  'backgroundSuccess': fccColors['green40'],
-  'backgroundInfo': fccColors['blue30'],
-  'backgroundSelection': fccColors['blue30Translucent'],
-};
+  static const backgroundPrimary = FccColors.gray90;
+  static const backgroundSecondary = FccColors.gray85;
+  static const backgroundTertiary = FccColors.gray80;
+  static const backgroundQuaternary = FccColors.gray75;
+  static const backgroundDanger = FccColors.red15;
+  static const backgroundSuccess = FccColors.green40;
+  static const backgroundInfo = FccColors.blue30;
+  static const backgroundSelection = FccColors.blue30Translucent;
+}
 
 class FccTheme {
   static ThemeData themeDark = ThemeData(
