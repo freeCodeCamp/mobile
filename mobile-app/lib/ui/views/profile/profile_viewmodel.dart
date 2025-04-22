71161303963c839b6cdbf9e16e8fe9f9b97b5004
Huyen Nguyen <25715018+huyenltnguyen@users.noreply.github.com>
2025-04-22 15:31:55 +0700

    chore: remove profile view changes

--- mobile-app/lib/ui/views/profile/profile_viewmodel.dart
+++ mobile-app/lib/ui/views/profile/profile_viewmodel.dart
@@ -2,6 +2,7 @@ import 'package:freecodecamp/app/app.locator.dart';
 import 'package:freecodecamp/service/authentication/authentication_service.dart';
 import 'package:stacked/stacked.dart';
 
+// import 'dart:developer';
 class ProfileViewModel extends BaseViewModel {
   final AuthenticationService auth = locator<AuthenticationService>();
 }
