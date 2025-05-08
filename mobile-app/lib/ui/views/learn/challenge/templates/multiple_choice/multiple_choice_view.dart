eb274c3a9ef8fd87f1036cb72d8a105b4990d978
Sem Bauke <semboot699@gmail.com>
2025-05-08 09:33:59 +0200

    feat: add multiple choice feedback and answer tracking (#1459)
    * feat: add multiple choice feedback and answer tracking
    
    * fix: selected radio button style
    
    * fix: make validation status independent of feedback message
    
    * fix: set feedback color based on validation status
    
    ---------
    
    Co-authored-by: Huyen Nguyen <25715018+huyenltnguyen@users.noreply.github.com>

--- mobile-app/lib/ui/views/learn/challenge/templates/multiple_choice/multiple_choice_view.dart
+++ mobile-app/lib/ui/views/learn/challenge/templates/multiple_choice/multiple_choice_view.dart
@@ -179,7 +179,10 @@ class MultipleChoiceView extends StatelessWidget {
                                   challenge,
                                   block,
                                 )
-                            : () => model.checkOption(challenge)
+                            : () {
+                                model.setValidationStatus(challenge);
+                                model.updateFeedback(challenge, context);
+                              }
                         : null,
                     child: Text(
                       model.choiceStatus != null
@@ -267,17 +270,16 @@ class MultipleChoiceView extends StatelessWidget {
     return Container(
       margin: const EdgeInsets.symmetric(vertical: 8),
       child: RadioListTile<int>(
+        key: ValueKey(model.lastAnswer),
         selected: answerObj.key == model.currentChoice,
         tileColor: const Color(0xFF0a0a23),
-        selectedTileColor: const Color(0xDEFFFFFF),
-        activeColor: const Color(0xFF0a0a23),
+        selectedTileColor: const Color(0xFF0a0a23),
+        activeColor: const Color(0xDEFFFFFF),
         value: answerObj.key,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(0),
           side: BorderSide(
-            color: answerObj.key == model.currentChoice
-                ? const Color(0xFF0a0a23)
-                : const Color(0xFFAAAAAA),
+            color: const Color(0xFFAAAAAA),
             width: 2,
           ),
         ),
@@ -291,13 +293,8 @@ class MultipleChoiceView extends StatelessWidget {
             Expanded(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children: parser.parse(
-                  answerObj.value.answer,
-                  isSelectable: false,
-                  fontColor: answerObj.key == model.currentChoice
-                      ? const Color(0xFF0a0a23)
-                      : null,
-                ),
+                children: parser.parse(answerObj.value.answer,
+                    isSelectable: false, fontColor: const Color(0xDEFFFFFF)),
               ),
             ),
             SizedBox(
@@ -314,6 +311,11 @@ class MultipleChoiceView extends StatelessWidget {
             ),
           ],
         ),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [if (answerObj.key == model.lastAnswer) ...model.feedback],
+        ),
+        isThreeLine: true,
       ),
     );
   }
