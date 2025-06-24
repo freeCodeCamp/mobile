0356aa2d7ad3ec74827c6edf1fbb849963dc8fac
Huyen Nguyen <25715018+huyenltnguyen@users.noreply.github.com>
2025-06-24 17:57:07 +0700

    fix(view): challenge title (#1537)
    * fix: title display
    
    * fix: handleChallengeTitle should return an empty string for step-based challenges

--- mobile-app/lib/ui/views/learn/challenge/templates/python/python_view.dart
+++ mobile-app/lib/ui/views/learn/challenge/templates/python/python_view.dart
@@ -3,6 +3,7 @@ import 'package:freecodecamp/extensions/i18n_extension.dart';
 import 'package:freecodecamp/models/learn/challenge_model.dart';
 import 'package:freecodecamp/models/learn/curriculum_model.dart';
 import 'package:freecodecamp/ui/views/learn/challenge/templates/python/python_viewmodel.dart';
+import 'package:freecodecamp/ui/views/learn/utils/challenge_utils.dart';
 import 'package:freecodecamp/ui/views/learn/widgets/quiz_widget.dart';
 import 'package:freecodecamp/ui/views/learn/widgets/youtube_player_widget.dart';
 import 'package:freecodecamp/ui/views/news/html_handler/html_handler.dart';
@@ -33,9 +34,7 @@ class PythonView extends StatelessWidget {
       builder: (context, model, child) {
         return Scaffold(
           appBar: AppBar(
-            title: Text(
-              '$currentChallengeNum of ${block.challenges.length} Questions',
-            ),
+            title: Text(handleChallengeTitle(challenge, block)),
           ),
           body: SafeArea(
             bottom: false,
