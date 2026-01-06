4873985721a73f902cd23db0eafb6233c4a00e16
Huyen Nguyen <25715018+huyenltnguyen@users.noreply.github.com>
2026-01-07 00:31:12 +0700

    feat: add A1 Chinese and A1 Spanish icons

--- mobile-app/lib/ui/views/learn/landing/landing_view.dart
+++ mobile-app/lib/ui/views/learn/landing/landing_view.dart
@@ -241,8 +241,8 @@ class SuperBlockButton extends StatelessWidget {
     SuperBlocks.a2English: '${SuperBlockButton.learnAssetsPath}/a2-english.svg',
     SuperBlocks.b1English: '${SuperBlockButton.learnAssetsPath}/b1-english.svg',
     SuperBlocks.a2Spanish: '${SuperBlockButton.learnAssetsPath}/a2-english.svg',
-    SuperBlocks.a1Spanish: '${SuperBlockButton.learnAssetsPath}/a2-english.svg',
-    SuperBlocks.a1Chinese: '${SuperBlockButton.learnAssetsPath}/a2-english.svg',
+    SuperBlocks.a1Spanish: '${SuperBlockButton.learnAssetsPath}/a1-spanish.svg',
+    SuperBlocks.a1Chinese: '${SuperBlockButton.learnAssetsPath}/a1-chinese.svg',
     SuperBlocks.rosettaCode:
         '${SuperBlockButton.learnAssetsPath}/rosetta-code.svg',
     SuperBlocks.pythonForEverybody:
