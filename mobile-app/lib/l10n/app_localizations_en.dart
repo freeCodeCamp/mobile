bec2d094d696474d06fb6f8e489a188f3b230ed9
Niraj Nandish <nirajnandish@icloud.com>
2026-06-26 07:19:50 +0530

    fix: daily challenges sidebar casing

--- mobile-app/lib/l10n/app_localizations_en.dart
+++ mobile-app/lib/l10n/app_localizations_en.dart
@@ -547,7 +547,7 @@ class AppLocalizationsEn extends AppLocalizations {
   }
 
   @override
-  String get quick_action_daily_challenges => 'Daily Challenges';
+  String get quick_action_daily_challenges => 'DAILY CHALLENGES';
 
   @override
   String get code_radio => 'CODE RADIO';
